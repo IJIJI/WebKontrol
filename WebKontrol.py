8ae2409b1db3e18c1b2c0fd82ec63ebf60b2a133
IJssel <54723855+IJIJI@users.noreply.github.com>
2023-04-27 18:00:48 +0200

    Update WebKontrol.py

--- WebKontrol.py
+++ WebKontrol.py
@@ -2,6 +2,8 @@ from puppet import webPuppet
 from admin import webAdmin
 import time
 
+puppet = webPuppet()
+puppet.start()
 
 admin = webAdmin()
 admin.start()
@@ -10,6 +12,7 @@ while(1):
     try:
         time.sleep(0.01)
         if (postvalue := admin.getPostValue()) != None:
+            puppet.set_url(postvalue)
             print("New value: " + postvalue)
     except KeyboardInterrupt:
         puppet.stop()
