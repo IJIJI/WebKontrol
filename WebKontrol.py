5fb767b233db72bbd86460fa486c797260a52d2d
IJssel <54723855+IJIJI@users.noreply.github.com>
2023-04-28 19:20:02 +0200

    added config

--- WebKontrol.py
+++ WebKontrol.py
@@ -3,13 +3,25 @@ from admin import webAdmin
 from config import webConfig
 import time
 
+config = webConfig()
+
+
 puppet = webPuppet()
 puppet.update()
 
 admin = webAdmin()
 admin.start()
 
-admin.set_url(puppet.get_url())
+if config.check_config():
+    config.load()
+    puppet.set_url(config.get_url())
+    admin.set_url(puppet.get_url())
+    puppet.update()
+else:
+    config.set_url(puppet.get_url())
+    admin.set_url(puppet.get_url())
+    config.save()
+
 
 while(1):
     try:
@@ -21,6 +33,8 @@ while(1):
             puppet.set_url(postvalue)
             if(puppet.update()):
                 admin.set_url(puppet.get_url())
+                config.set_url(puppet.get_url())
+                config.save()
                 print("New URL: " + postvalue)
             else:
                 time.sleep(4)
--- config.py
+++ config.py
@@ -1,4 +1,34 @@
+import json
+
 class webConfig:
 
-    def __init__(self) -> None:
-        pass
+    url = ""
+
+    def check_config(self):
+        try:
+            with open('config.json') as json_file:
+                data = json.load(json_file)
+        except FileNotFoundError:
+            return False
+        return True
+
+    def load(self):
+        with open('config.json') as json_file:
+            data = json.load(json_file)
+            self.url = data['url']
+        return
+
+    def save(self):
+        data = {}
+        data['url'] = self.url
+        with open('config.json', 'w') as outfile:
+            json.dump(data, outfile)
+        return
+
+    def set_url(self, url):
+        self.url = url
+        return
+
+    def get_url(self):
+        return self.url
+
