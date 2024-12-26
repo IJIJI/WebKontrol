8902b2590a8da0c8fa0ce89bda39cc0a96628b4f
IJssel Koster <ij.koster@student.tudelft.nl>
2024-12-26 17:36:30 +0100

    Added routes and pug

--- src/src/WebServer.ts
+++ src/src/WebServer.ts
@@ -25,13 +25,33 @@ export class WebServer extends EventEmitter {
   constructor() {
   // constructor(settingLoader: SettingLoader) {
     super();
-    this.app.get('/', (req, res) => {
-      res.send('Hello World! boo')
-    })    
+
+    this.app.set('view engine', 'pug');
+    this.app.set('views', './views');
 
     this.app.use('/static', express.static('static'));
     // this.app.use('/static', express.static(path.join(__dirname, 'static')))
 
+
+    this.app.get('/', (req, res) => {
+      res.render('index', { title: 'Hey', message: 'Hello there!' })
+    });
+
+    this.app.get('/updated', (req, res) => {
+      // this.emit('sourceChange');
+      res.send('Updated');
+    });
+
+    // PUPPET SIDE
+    this.app.get('/clock', (req, res) => {
+      res.render('clock', { title: 'Clock' })
+    });
+    this.app.get('/no_connect', (req, res) => {
+      res.render('no_connect', {})
+    });
+    this.app.get('/splash', (req, res) => {
+      res.render('splash', {})
+    });
   }
 
   start() {
