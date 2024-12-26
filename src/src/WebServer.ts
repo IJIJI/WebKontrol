e39b06c56f16a37bbc872e0ca79ee00b29d978cb
IJssel <ijssel@synapt.nl>
2024-12-26 18:19:33 +0100

    Implemented twig and started templating

--- src/src/WebServer.ts
+++ src/src/WebServer.ts
@@ -1,6 +1,7 @@
 import { EventEmitter } from "events";
 import express from 'express';
 import path from 'path';
+import Twig from 'twig';
 
 export interface WebServerEvents {
   sourceChange: () => void;
@@ -26,16 +27,35 @@ export class WebServer extends EventEmitter {
   // constructor(settingLoader: SettingLoader) {
     super();
 
-    this.app.set('view engine', 'pug');
-    this.app.set('views', './views');
+    // this.app.set('view engine', 'pug');
+    // this.app.set('views', './views');
+    this.app.set("twig options", {
+      allowAsync: true, // Allow asynchronous compiling
+      strict_variables: false
+    });
+
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: true }));
 
     this.app.use('/static', express.static('static'));
     // this.app.use('/static', express.static(path.join(__dirname, 'static')))
 
 
     this.app.get('/', (req, res) => {
-      res.render('index', { title: 'Hey', message: 'Hello there!' })
+      // res.render('index', { title: 'Hey', message: 'Hello there!' })
+      res.render('index.html.twig', {
+        cur_url : "Hello World",
+        cur_url_short: "Bye"
+      });
     });
+
+    this.app.post('/set_url', (req, res) => {
+      // res.send(req.bod);
+      console.log('POST parameter received are: ',req.body);
+      res.render('updated.html.twig', {
+      });
+    });
+
 
     this.app.get('/updated', (req, res) => {
       // this.emit('sourceChange');
@@ -44,13 +64,16 @@ export class WebServer extends EventEmitter {
 
     // PUPPET SIDE
     this.app.get('/clock', (req, res) => {
-      res.render('clock', { title: 'Clock' })
+      res.render('clock.html.twig', {
+      });
     });
     this.app.get('/no_connect', (req, res) => {
       res.render('no_connect', {})
     });
     this.app.get('/splash', (req, res) => {
-      res.render('splash', {})
+      res.render('splash.html.twig', {
+        admin_url : ["google.com", "synapt.eu"],
+      });
     });
   }
 
