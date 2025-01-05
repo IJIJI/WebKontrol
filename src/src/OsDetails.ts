583b2baf193db03e4578e3fcb8783585e8ed3526
IJssel <ijssel@synapt.nl>
2025-01-05 23:17:20 +0100

    Formatted ip from osdetails

--- src/src/OsDetails.ts
+++ src/src/OsDetails.ts
@@ -11,20 +11,20 @@ export class OsDetails {
     const addresses: string[] = [];
     
     Object.keys(ifaces).forEach(function (ifname) {
-       var alias = 0;
-    
-    
-       ifaces[ifname].forEach(function (iface) {
-          if ('IPv4' !== iface.family || iface.internal !== false) {
-             // Skip over internal (i.e. 127.0.0.1) and non-IPv4 addresses
-             return;
-          }
-    
-          addresses.push(iface.address);
-    
-       });
+      var alias = 0;
+      
+      
+      ifaces[ifname].forEach(function (iface) {
+        if ('IPv4' !== iface.family || iface.internal !== false) {
+          // Skip over internal (i.e. 127.0.0.1) and non-IPv4 addresses
+          return;
+        }
+        
+        addresses.push("http://" + iface.address + "/");
+        
+      });
     });
-
+    
     return addresses;
   }
 }
