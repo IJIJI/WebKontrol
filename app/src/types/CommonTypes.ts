ce36804aa77e31c10bdea05c7af6314c9b4ae94d
IJssel <ijssel@synapt.nl>
2026-06-25 20:02:51 +0200

    Added CommonTypes

---
+++ app/src/types/CommonTypes.ts
@@ -0,0 +1,22 @@
+export interface DisplayName {
+  long: string;
+  short?: string;
+}
+
+export enum ConnectionState {
+  DISABLED = "Disabled",
+  OFFLINE = "Offline",
+  ONLINE = "Online",
+  ERROR = "Error",
+  FAILED = "Failed",
+}
+
+export type WithRequired<T, K extends keyof T> = Partial<T> &
+  Required<Pick<T, K>>;
+
+
+export abstract class CommonTools {
+  static areDisplayNamesEqual(a: DisplayName, b: DisplayName): boolean {
+    return a.long === b.long && a.short === b.short;
+  }
+}
