2bc7e3b9badfcd083e200896e0b7fcbc7bdf7955
IJssel <ijssel@synapt.nl>
2026-06-25 19:51:36 +0200

    Started on abspuppet functions

--- app/src/puppet/AbstractPuppet.ts
+++ app/src/puppet/AbstractPuppet.ts
@@ -41,4 +41,56 @@ export abstract class AbstractPuppet<
   }
 
   protected abstract _doInit(): Promise<void>;
+
+  async setTarget(target: PuppetTarget): Promise<SetTargetResult> {
+    this._target_url = target;
+    try {
+      const success = await this._doSetTarget(target);
+      if (success) {
+        const result: SetTargetSuccess = {
+          success: true,
+          info: {
+            url: this._target_url,
+
+            // title: this._info.title, // TODO, also description and og
+          },
+        };
+        // this._emitInfoUpdate(result.info); //TODO
+        (this as EventEmitter<PuppetEvents>).emit("load_success", result);
+        return result;
+      } else {
+        throw new Error("Failed to set target with unknown error!");
+      }
+    }
+    catch (error) {
+      this._logger.error("Failed to set target", error);
+      const result: SetTargetFail = {
+        success: false,
+        info: {
+          url: this._target_url,
+
+          // title: this._info.title, // TODO, also description and og
+        },
+      };
+      if (error instanceof Error) {
+        result.error = error;
+      }
+      // this._emitInfoUpdate(result.info); //TODO
+      (this as EventEmitter<PuppetEvents>).emit("load_fail", result);
+      return result;
+    }
+
+  }
+
+  protected _emitInfoUpdate(info: PuppetInfo) {
+    (this as EventEmitter<PuppetEvents>).emit("info_update", info);
+  }
+
+  protected abstract _doSetTarget(target: PuppetTarget): Promise<boolean>;
+
+  async getInfo(): Promise<PuppetInfo> {
+    return { ...this._info, target: await this._getTargetInfo() };
+  }
+
+  protected abstract _getTargetInfo(): Promise<TargetInfo> | TargetInfo;
 }
