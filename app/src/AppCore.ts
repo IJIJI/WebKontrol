886ad057fc6747733f73c3c8f17d22923af5c1fd
IJssel <ijssel@synapt.nl>
2026-06-25 18:44:00 +0200

    added puppetmap

--- app/src/AppCore.ts
+++ app/src/AppCore.ts
@@ -1,4 +1,7 @@
 import { Logger } from "./logging/Logger";
+import type { AbstractPuppet } from "./puppet/AbstractPuppet";
+import type { Puppet } from "./puppet/PuppeteerPuppet";
+import type { PuppetKey } from "./puppet/types";
 
 
 
@@ -8,6 +11,8 @@ export interface CoreInfo {
 
 export class AppCore {
   private logger = new Logger(["CORE"]);
+
+  private puppets: Map<PuppetKey, AbstractPuppet> = new Map();
   
   private info: CoreInfo = {
     startTime: Date.now()
