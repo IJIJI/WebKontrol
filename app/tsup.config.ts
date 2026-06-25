7111306241b79b2c7df599276473148e79f52311
IJssel <ijssel@synapt.nl>
2026-06-25 17:49:19 +0200

    added tsup config

---
+++ app/tsup.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: { app: "app.ts", supervisor: "supervisor.ts" },
+  format: ["esm"],
+  outDir: "dist",
+  platform: "node",
+  splitting: false,
+  clean: false,
+  external: ["pigpio"],
+});
