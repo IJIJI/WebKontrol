f7c4e4a1b8d67b449398375d66bf0653adfa9ab9
IJssel <ijssel@synapt.nl>
2026-06-25 22:22:52 +0200

    Added testscript

--- app/src/AppCore.ts
+++ app/src/AppCore.ts
@@ -1,5 +1,6 @@
 import { Logger } from "./logging/Logger";
 import type { AbstractPuppet } from "./puppet/AbstractPuppet";
+import { PuppeteerPuppet } from "./puppet/PuppeteerPuppet";
 import type { PuppetKey } from "./puppet/types";
 
 export interface CoreInfo {
@@ -19,5 +20,21 @@ export class AppCore {
 
   public async start() {
     this.logger.info("Starting AppCore...");
+
+    const testpuppet = new PuppeteerPuppet({
+      id: "test",
+      name: "Test Puppet",
+      target_url: "https://example.com",
+    });
+
+    await testpuppet.init();
+
+
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    await testpuppet.setTarget("https://synapt.net/");
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    await testpuppet.setTarget("https://synaapt.net/");
+
+    this.puppets.set(testpuppet.getConfig().id, testpuppet);
   }
 }
