ee7f532f4f0275ab4f2e69c18cfdc10856fb4d0a
IJssel <ijssel@synapt.nl>
2026-06-25 19:50:31 +0200

    Puppet types

--- app/src/puppet/types.ts
+++ app/src/puppet/types.ts
@@ -2,3 +2,38 @@
 
 export type PuppetKey = string;
 export type PuppetTarget = string;
+
+export interface PuppetInfo {
+  target: TargetInfo;
+}
+
+export type TargetInfo = {
+  url: PuppetTarget;
+  title?: string;
+  description?: string;
+  og?: OgTargetInfo;
+};
+
+export type OgTargetInfo = {
+  title?: string;
+  description?: string;
+  image?: string;
+};
+
+
+export type SetTargetFail = {
+  success: false;
+  info: TargetInfo;
+  error?: Error;
+};
+
+export type SetTargetSuccess = {
+  success: true;
+  info: TargetInfo;
+};
+
+export type SetTargetResult = SetTargetSuccess | SetTargetFail;
+
+export interface PuppetConfig {
+  // Define the structure of your PuppetConfig here
+}
