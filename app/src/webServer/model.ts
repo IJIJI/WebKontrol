f97a043d53a86b233876a6ccd3f970fd7017eac3
IJssel <ijssel@synapt.nl>
2026-06-28 01:35:59 +0200

    Propegated system types

--- app/src/webServer/model.ts
+++ app/src/webServer/model.ts
@@ -1,11 +1,19 @@
 import type { PuppetInfoBundle } from "../puppet/model";
 import type { PuppetKey, PuppetRuntimeConfigInput } from "../puppet/schema";
+import type { SystemInfo } from "../system/model";
+import type { SystemConfig } from "../system/schema";
 
 
 export interface WebServerState {
   puppets: PuppetInfoBundle[];
+  system: {
+    info: SystemInfo,
+    config: SystemConfig,
+  }
 }
 
 export interface WebServerMutationHandlers {
   setPuppetRuntime: (id: PuppetKey, runtime: PuppetRuntimeConfigInput) => Promise<void>;
 }
+
+
