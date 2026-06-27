b31fc285672859b643830711304afc741f9e4fe6
IJssel <ijssel@synapt.nl>
2026-06-27 17:11:11 +0200

    Added model doc

---
+++ app/src/puppet/model.ts
@@ -0,0 +1,4 @@
+/**
+ * @fileoverview This file contains all model definitions for the puppets. That means types that don't need validation. 
+ * Those are types that are internally created, and can be blindly trusted to be valid.
+ */
