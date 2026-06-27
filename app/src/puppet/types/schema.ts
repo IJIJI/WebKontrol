0cd146bb902aa8a174632c37ab3f99e9d27e972a
IJssel <ijssel@synapt.nl>
2026-06-27 17:40:39 +0200

    Moved new type files and more clear comment

--- app/src/puppet/types/schema.ts
+++ app/src/puppet/types/schema.ts
@@ -1,4 +1,4 @@
 /**
- * This file contains all schema definitions for the puppets. That means the validated types. 
+ * This file contains all schema definitions for the puppets. That means the types need to be validated. 
  * Those are types that are not generated or received from the internal code, and should thus not automatically be trusted.
  */
