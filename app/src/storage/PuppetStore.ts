a5b42b46d8cc98da70c2561c1a138eca73b557f8
IJssel <ijssel@synapt.nl>
2026-06-27 21:26:51 +0200

    Added store logging

--- app/src/storage/PuppetStore.ts
+++ app/src/storage/PuppetStore.ts
@@ -16,17 +16,26 @@ export class PuppetStore<T extends PuppetRuntimeConfig = PuppetRuntimeConfig> {
     this._id = id;
     this._schema = schema;
     this._logger = new Logger(["STORE", "PUPPET", id]);
+    this._logger.debug(`Constructed runtime.`);
   }
 
   public async saveRuntime(config: T): Promise<void> {
+    this._logger.debug(`Saving runtime...`);
     await this._db.updateSetting("puppet", this._id, "runtime", JSON.stringify(config));
+    this._logger.debug(`Successfully saved runtime!`);
   }
 
   public async loadRuntime(): Promise<T | null> {
     try {
+      this._logger.debug(`Loading runtime...`);
       const raw = await this._db.getSetting("puppet", this._id, "runtime");
-      if (raw === null) return null;
-      return this._schema.parse(JSON.parse(raw));
+      if (raw === null) {
+        this._logger.error(`Failed loading runtime! Got null`)
+        return null;
+      }
+      const object = this._schema.parse(JSON.parse(raw))
+      this._logger.debug(`Successfully loaded runtime!`, object);
+      return object;
     } catch (error) {
       this._logger.error(`Failed loading runtime for puppet ${this._id} with error:`, error);
       return null;
