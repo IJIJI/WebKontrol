8923fc363aa9f8545d2cb8b3f0834171710dd1ad
IJssel <ijssel@synapt.nl>
2026-06-28 01:59:00 +0200

    Adding destroy code

--- app/src/webServer/WebServer.ts
+++ app/src/webServer/WebServer.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import ViteExpress from "vite-express";
+import http from 'http';
 import { Logger } from "../logging/Logger";
 import type { WebServerMutationHandlers, WebServerState } from "./model";
 import { jsonReplacer } from "../helpers/json";
@@ -10,6 +11,7 @@ import { WebServerConfigSchema, type WebServerConfig, type WebServerConfigInput 
 
 export class WebServer {
   private _app = express();
+  private _server: http.Server;
   private _logger = new Logger(["WEBSERVER"]);
 
   private _config: WebServerConfig;
@@ -58,7 +60,7 @@ export class WebServer {
     this._handlers = handlers;
   }
 
-  public start(config: WebServerConfig) {
+  public async start(config: WebServerConfig): Promise<void> {
     if (this._handlers !== undefined) {
       // TODO: Continue without and set state to ERROR, until they are set?
       throw new Error("Handlers where not set before the server was started!"); // TODO: Check if this should error. Check if there should be an info for the state. 
@@ -74,12 +76,52 @@ export class WebServer {
       verbosity: ViteExpress.Verbosity.Silent,
       mode: process.env.NODE_ENV === 'production' ? 'production' : 'development'
     });
+
     this._registerRoutes();
 
-    ViteExpress.listen(this.app, this._config, () => {
-      this._logger.info(`Admin server running on http://localhost:${port}`);
+    this._server = await new Promise((res) => {
+      const server = ViteExpress.listen(this._app, this._config.port, () => {
+        this._logger.info(`Admin server running on http://localhost:${this._config.port}`);
+        res(server);
+      });
     });
 
     this._logger.info("Admin server started.");
+  }
+
+  public async destroy(): Promise<void> {
+    if (!this._isInit) {
+      this._logger.warn("Tried destroying an uninitialised WebServer. Ignoring.");
+    }
+
+    this._isInit = false; // TODO: Add isDestroying variables?
+
+    this._logger.info("Attempting graceful shutdown...");
+
+    await new Promise<void>((resolve, reject) => {
+      const forceCloseTimeout = setTimeout(() => {
+        this._logger.warn("Shutdown timeout reached. Forcing exit.");
+        // Forcefully close all connections
+        this._server?.closeAllConnections();
+      }, 3000);
+
+      const forceRejectTimeout = setTimeout(() => {
+        this._logger.warn("Destroy timeout reached. Marking as unsuccessfull.");
+        reject(new Error("Failed to destroy within timeout."))
+      }, 5000);
+
+      this._server!.close((err) => {
+        clearTimeout(forceCloseTimeout);
+        clearTimeout(forceRejectTimeout);
+        
+        if (err) {
+          this._logger.error("Error during server shutdown", err);
+          return reject(err);
+        }
+        
+        this._logger.info("Server closed successfully.");
+        resolve();
+      });
+    });
   }
 }
