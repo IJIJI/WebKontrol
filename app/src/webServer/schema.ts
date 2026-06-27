8e0630987b8b8d86ab6743254485e50b534811b4
IJssel <ijssel@synapt.nl>
2026-06-28 01:36:07 +0200

    Added system schema

---
+++ app/src/webServer/schema.ts
@@ -0,0 +1,8 @@
+import z from "zod";
+
+export const WebServerConfigSchema = z.object({
+  port: z.number().gte(0).lte(65535).default(80),
+});
+
+export type WebServerConfig = z.infer<typeof WebServerConfigSchema>
+export type WebServerConfigInput = z.input<typeof WebServerConfigSchema>
