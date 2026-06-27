dd5c16425690cf054f8ba5884313a896448a7d6a
IJssel <ijssel@synapt.nl>
2026-06-27 16:52:49 +0200

    started model schema split

---
+++ app/src/puppet/schema.ts
@@ -0,0 +1,4 @@
+/**
+ * @fileoverview This file contains all schema definitions for the puppets. That means the validated types. 
+ * Those are types that are not generated or received from the internal code, and should thus not automatically be trusted.
+ */
