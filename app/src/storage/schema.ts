4b24681ac813e2d9ef78a85e27ec84586a4c9462
IJssel <ijssel@synapt.nl>
2026-06-27 16:17:44 +0200

    Added global db key

--- app/src/storage/schema.ts
+++ app/src/storage/schema.ts
@@ -8,15 +8,17 @@ import { createInsertSchema } from 'drizzle-zod';
 // });
 
 export const settings = sqliteTable("settings", {
+  domain: text('domain').notNull(),
   type: text('type').notNull(),
   key: text('key').notNull(),
   value: text('value').notNull(),
 }, (table) => [
-  primaryKey({ columns: [table.type, table.key] }),
+  primaryKey({ columns: [table.domain, table.type, table.key] }),
 ]);
 
 // Automatically generate a Zod schema for inserting data, with custom runtime validation.
 export const insertSettingSchema = createInsertSchema(settings, {
+  domain: (schema) => schema.min(3).max(50),
   type: (schema) => schema.min(3).max(50),
   key: (schema) => schema.min(1).max(50),
   value: (schema) => schema.min(1, "Value cannot be empty"),
