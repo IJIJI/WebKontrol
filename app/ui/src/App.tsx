0b484896abfcfe5df464453b422779179e9bd1ac
IJssel <ijssel@synapt.nl>
2026-06-28 15:38:43 +0200

    Import cleanup

--- app/ui/src/App.tsx
+++ app/ui/src/App.tsx
@@ -1,4 +1,4 @@
-import { JSX } from "react";
+import type { JSX } from "react";
 import { AppErrorBoundary } from "./boundaries/AppErrorBoundary";
 import { ApiStateProvider } from "./context/ApiStateContext";
 
