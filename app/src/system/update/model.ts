79baadfc4175c37c1de1f79843b98a9f42ceefa5
IJssel <ijssel@synapt.nl>
2026-06-28 02:15:24 +0200

    Added interface split todo

--- app/src/system/update/model.ts
+++ app/src/system/update/model.ts
@@ -12,6 +12,7 @@ export interface GitHubBranch {
   name: string;
 }
 
+// TODO: Split into get available updates and system status? There is a checkupdates and getstatus function anyways.
 export interface UpdateStatus {
   current:     string;
   releases:    GitHubRelease[];
