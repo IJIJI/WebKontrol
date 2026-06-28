3f694e96ec5a194fa7fdabc5a4fbb37df0657fee
IJssel <ijssel@synapt.nl>
2026-06-28 13:23:02 +0200

    Better order

--- app/src/puppet/validation.ts
+++ app/src/puppet/validation.ts
@@ -7,15 +7,15 @@ export const IncomingPuppetSpecificConfigSchema = z.discriminatedUnion("type", [
 
 export type AnyPuppetSpecificConfig = z.infer<typeof IncomingPuppetSpecificConfigSchema>;
 
+export const IncomingPuppetConfigSchema = z.discriminatedUnion("type", [
+  PuppeteerPuppetConfigSchema,
+]);
+
 export const ConfigFilePuppetSpecificSchema = z.preprocess(
   (data) => (typeof data === 'object' && data !== null && !('type' in data))
     ? { ...(data as Record<string, unknown>), type: 'puppeteer' }
     : data,
   IncomingPuppetSpecificConfigSchema
 );
-
-export const IncomingPuppetConfigSchema = z.discriminatedUnion("type", [
-  PuppeteerPuppetConfigSchema,
-]);
 
 export type AnyPuppetConfig = z.infer<typeof IncomingPuppetConfigSchema>;
