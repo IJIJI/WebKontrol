e3762cff4fd79f9035146395deba3db7392c68cd
IJssel <ijssel@synapt.nl>
2026-06-28 14:20:20 +0200

    Started on provider

--- app/ui/src/context/ApiStateProvider.tsx
+++ app/ui/src/context/ApiStateProvider.tsx
@@ -1,9 +1,26 @@
 import { ReactNode } from "react";
+import { WebServerState } from "../../../src/webServer/model"
+import { PuppetKey, PuppetRuntimeConfigInput } from "../../../src/puppet/schema";
+import { SystemConfig } from "../../../src/system/schema";
 
 
-
-
-
+// TODO: This is pretty much the same as the webserver mutation handlers, but I don't think it will be in the future. Check how to best keep in sync.
+interface ApiState {
+  state: WebServerState;
+  callBacks: {
+    puppet: {
+      setRuntime: (id: PuppetKey, runtime: PuppetRuntimeConfigInput) => Promise<void>;
+    },
+    system: {
+      setConfig: (config: SystemConfig) => Promise<void>;
+      // update: {
+      //   check: () => Promise<void>; // (return type was UpdateStatus) // TODO: Split update status into current and available or smt
+      //   apply: (ref: string, type: 'release' | 'branch') => Promise<void>; // TODO: Check arguments
+      //   getStatus: () => Promise<void>; // (return type was UpdateStatus) // TODO: Split update status into current and available or smt
+      // }
+    }
+  }
+}
 
 
 
