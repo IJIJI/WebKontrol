e12d8497fbc538af8ca20848f0f3fa49f9749fbd
IJssel <ijssel@synapt.nl>
2026-06-28 13:29:43 +0200

    added default config type todo

--- app/src/puppet/validation.ts
+++ app/src/puppet/validation.ts
@@ -11,6 +11,7 @@ export const IncomingPuppetConfigSchema = z.discriminatedUnion("type", [
   PuppeteerPuppetConfigSchema,
 ]);
 
+// TODO: Feels brittle. Add type on base config as default puppeteer?
 export const ConfigFilePuppetSpecificSchema = z.preprocess(
   (data) => (typeof data === 'object' && data !== null && !('type' in data))
     ? { ...(data as Record<string, unknown>), type: 'puppeteer' }
