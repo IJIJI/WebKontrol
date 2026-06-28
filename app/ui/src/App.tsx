b038d74ae6518fc40d00619079a1674234bb24bf
IJssel <ijssel@synapt.nl>
2026-06-28 16:32:03 +0200

    Started ViewStateProvider

--- app/ui/src/App.tsx
+++ app/ui/src/App.tsx
@@ -1,13 +1,21 @@
 import type { JSX } from "react";
 import { AppErrorBoundary } from "./boundaries/AppErrorBoundary";
 import { ApiStateProvider } from "./context/ApiStateContext";
+import { ViewStateProvider } from "./context/ViewStateProvider";
 
 export default function App(): JSX.Element {
-  return (
-    <AppErrorBoundary>
-      <ApiStateProvider>
-        <h1>HI</h1>
-      </ApiStateProvider>
-    </AppErrorBoundary>
+  return ( //TODO: Check if this is the right split. Different error boundary for the views?
+    <>
+      <AppErrorBoundary>
+        <ApiStateProvider>
+          <h1>HI</h1>
+        </ApiStateProvider>
+      </AppErrorBoundary>
+      <AppErrorBoundary>
+        <ViewStateProvider>
+          <h1>Screen!</h1>
+        </ViewStateProvider>
+      </AppErrorBoundary>
+    </>
   );
 }
