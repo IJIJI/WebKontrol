cc78a7b52418bda2f02e583d8366a6684c3d5fc4
IJssel <ijssel@synapt.nl>
2026-06-29 00:20:40 +0200

    Added AppCoreStore

---
+++ app/src/storage/AppCoreStore.ts
@@ -0,0 +1,45 @@
+import { Logger } from "../logging/Logger";
+import { SystemConfigSchema, type SystemConfig } from "../system/schema";
+import { CoreDatabase } from "./CoreDatabase";
+
+export class AppCoreStore {
+
+  private _db = CoreDatabase.getInstance();
+  private _logger: Logger;
+
+  constructor() {
+    this._logger = new Logger(["STORE", "CORE"]);
+    this._logger.debug(`Constructed store.`);
+  }
+
+  public async saveRuntime(config: SystemConfig): Promise<void> {
+    this._logger.debug(`Saving runtime...`);
+    await this._db.updateSetting(
+      "app",
+      "core",
+      "runtime",
+      JSON.stringify(config),
+    );
+    this._logger.debug(`Successfully saved runtime!`);
+  }
+
+  public async loadRuntime(): Promise<SystemConfig | null> {
+    try {
+      this._logger.debug(`Loading runtime...`);
+      const raw = await this._db.getSetting("app", "core", "runtime");
+      if (raw === null) {
+        this._logger.error(`Failed loading runtime! Got null`);
+        return null;
+      }
+      const object = SystemConfigSchema.parse(JSON.parse(raw));
+      this._logger.debug(`Successfully loaded runtime!`, object);
+      return object;
+    } catch (error) {
+      this._logger.error(
+        `Failed loading runtime with error:`,
+        error,
+      );
+      return null;
+    }
+  }
+}
