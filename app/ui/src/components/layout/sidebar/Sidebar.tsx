f111e5f74b01156320b09a46074504a6d872fdb4
IJssel <ijssel@synapt.nl>
2026-06-30 17:43:50 +0200

    Added sidebar params and some content

--- app/ui/src/components/layout/sidebar/Sidebar.tsx
+++ app/ui/src/components/layout/sidebar/Sidebar.tsx
@@ -1,5 +1,9 @@
 import type { JSX } from "react/jsx-runtime";
+import { DeviceType } from "../PageLayout";
 
-export default function Sidebar(): JSX.Element {
-  return(<></>);
+export default function Sidebar({isCollapsed, setCollapsed, deviceType, className}: {isCollapsed: boolean, setCollapsed: (state: boolean) => void, deviceType: DeviceType, className?: string }): JSX.Element {
+  return(
+    <div className={["sidebar", deviceType == DeviceType.MOBILE && "mobile", className].filter(Boolean).join(" ")}>
+    </div>
+  );
 }
