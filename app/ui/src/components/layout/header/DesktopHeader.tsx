c1487d8f25b6a5891b819c7c94ed1bc11a0dfa4a
IJssel <ijssel@synapt.nl>
2026-06-30 22:39:36 +0200

    Tweaked content padding

--- app/ui/src/components/layout/header/DesktopHeader.tsx
+++ app/ui/src/components/layout/header/DesktopHeader.tsx
@@ -7,7 +7,7 @@ import ContentSection from "../content/ContentSection";
 export default function DesktopHeader({version, setCollapsed, className}: {version: string, setCollapsed: (state: boolean) => void, className?: string }): JSX.Element {
   // TODO logo a link and autocollapse.
   return(
-    <ContentSection variant="glass" className={["header", "desktop", "pad-s", className].filter(Boolean).join(" ")} >
+    <ContentSection variant="glass" className={["header", "desktop", "pad-m", className].filter(Boolean).join(" ")} >
       <BrandLogo size={30} version={version} /> 
     </ContentSection>
     
