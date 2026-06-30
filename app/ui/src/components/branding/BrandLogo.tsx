c196056bf6ffc1255cef949b4eb3986d758daaad
IJssel <ijssel@synapt.nl>
2026-06-30 23:13:08 +0200

    started on brandLogo collapse animation

--- app/ui/src/components/branding/BrandLogo.tsx
+++ app/ui/src/components/branding/BrandLogo.tsx
@@ -2,33 +2,29 @@ import type { JSX } from "react/jsx-runtime";
 
 import "./brandLogo.less";
 
-type LogoProps = { 
-  size?: number, 
+type LogoProps = {
+  size?: number,
   subtext?: boolean,
+  collapsed?: boolean,
   version: string
 }
 
-export function BrandLogo({size = 26, subtext = true, version}: LogoProps): JSX.Element {
+export function BrandLogo({size = 26, subtext = true, collapsed = false, version}: LogoProps): JSX.Element {
+  const trail = collapsed ? " collapsed" : "";
   return(<div className="brandLogo">
     <h1 className="projectName" style={{fontSize: size}}>
-      <span className="web">
-        Web
+      <span className="word web">
+        W<span className={`trail ${trail}`}>eb</span>
       </span>
-      <span className="kontrol">
-        Kontrol
+      <span className="word kontrol">
+        K<span className={`trail ${trail}`}>ontrol</span>
       </span>
     </h1>
     { subtext &&
       <p className="subtext" style={{fontSize: size * 0.5, gap: size * 0.35}}>
-        <span className="version">
-          {version}
-        </span>
-        <span className="divider">
-        
-        </span>
-        <span className="producer">
-        Synapt
-        </span>
+        <span className="version">{version}</span>
+        <span className={`divider${trail}`}></span>
+        <span className={`producer${trail}`}>Synapt</span>
       </p>
     }
   </div>);
--- app/ui/src/components/layout/header/DesktopHeader.tsx
+++ app/ui/src/components/layout/header/DesktopHeader.tsx
@@ -4,11 +4,11 @@ import { BrandLogo } from "../../branding/BrandLogo";
 import "./header.less";
 import ContentSection from "../content/ContentSection";
 
-export default function DesktopHeader({version, setCollapsed, className}: {version: string, setCollapsed: (state: boolean) => void, className?: string }): JSX.Element {
+export default function DesktopHeader({version, collapsed = false, setCollapsed, className}: {version: string, collapsed?: boolean, setCollapsed: (state: boolean) => void, className?: string }): JSX.Element {
   // TODO logo a link and autocollapse.
   return(
     <ContentSection variant="glass" className={["header", "desktop", "pad-m", className].filter(Boolean).join(" ")} >
-      <BrandLogo size={30} version={version} /> 
+      <BrandLogo size={30} version={version} collapsed={collapsed} /> 
     </ContentSection>
     
   );
