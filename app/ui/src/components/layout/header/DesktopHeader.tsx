7e72e2234fce26a7488d73665a920a54f2664ea2
IJssel <ijssel@synapt.nl>
2026-06-30 17:42:49 +0200

    Added classnames to headers

--- app/ui/src/components/layout/header/DesktopHeader.tsx
+++ app/ui/src/components/layout/header/DesktopHeader.tsx
@@ -3,10 +3,10 @@ import { BrandLogo } from "../../branding/BrandLogo";
 
 import "./header.less";
 
-export default function DesktopHeader({version, setCollapsed}: {version: string, setCollapsed: (state: boolean) => void}): JSX.Element {
+export default function DesktopHeader({version, setCollapsed, className}: {version: string, setCollapsed: (state: boolean) => void, className?: string }): JSX.Element {
   // TODO logo a link and autocollapse.
   return(
-    <div className="header desktop">
+    <div className={["header", "desktop", className].filter(Boolean).join(" ")}>
       <BrandLogo size={30} version={version} /> 
     </div>
   );
