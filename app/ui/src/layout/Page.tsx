2afa7924b7f893315ddf5344d22c08213985a4d0
IJssel <ijssel@synapt.nl>
2026-06-30 13:53:48 +0200

    Added meta handeling to page helper

--- app/ui/src/layout/Page.tsx
+++ app/ui/src/layout/Page.tsx
@@ -1,10 +1,33 @@
 import { JSX, useEffect } from "react";
 
-const Page = ({children, title}: {children: JSX.Element, title?: string}) => {
+
+export type MetaData = {
+  title: string,
+  description: string,
+}
+
+export const DEFAULT_METADATA: MetaData = {
+  title: "WebKontrol",
+  description: "WebKontrol remote browser - An intuitive web kiosk with a web-based admin panel."
+}
+
+const withDefaultMeta = (meta?: Partial<MetaData>): MetaData => {
+  const combined = {
+    ...DEFAULT_METADATA,
+    ...meta,
+  }
+  if (meta?.title) {
+    combined.title = meta.title + " - WebKontrol";
+  }
+  return combined;
+}
+
+export const Page = ({children, meta}: {children: JSX.Element, meta?: MetaData}) => {
+  
   useEffect(() => {
-    document.title = title ? title + " - WebKontrol" : "WebKontrol";
-  }, [title]);
+    const parsed = withDefaultMeta(meta);
+
+    document.title = parsed.title;
+  }, [meta]);
   return children;
 };
-
-export default Page;
