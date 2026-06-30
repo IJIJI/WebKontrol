08de355ecb70e05713dbeec8140231ebcc1c3375
IJssel <ijssel@synapt.nl>
2026-06-30 14:29:58 +0200

    Changed prop handeling and structure

--- app/ui/src/components/branding/BrandLogo.tsx
+++ app/ui/src/components/branding/BrandLogo.tsx
@@ -1,20 +1,25 @@
 import { JSX } from "react/jsx-runtime";
 
 type LogoProps = { 
-  size: number, 
-  subtext: boolean,
+  size?: number, 
+  subtext?: boolean,
   version: string
 }
 
-export default function BrandLogo(props: LogoProps): JSX.Element {
+export function BrandLogo({size = 26, subtext = true, version}: LogoProps): JSX.Element {
   return(<div className="logo">
-    <h1 className="projectName" style={{fontSize: props.size}}>WebKontrol</h1>
-    { props.subtext &&
-      <p className="subtext" style={{fontSize: props.size * 0.2}}>
+    <h1 className="projectName" style={{fontSize: size}}>WebKontrol</h1>
+    { subtext &&
+      <p className="subtext" style={{fontSize: size * 0.4}}>
         <span className="version">
-          {props.version}
-          </span> 
-        - Synapt Technologies
+          {version}
+        </span>
+        <span className="divider">
+        -
+        </span>
+        <span className="producer">
+        Synapt Technologies
+        </span>
       </p>
     }
   </div>);
