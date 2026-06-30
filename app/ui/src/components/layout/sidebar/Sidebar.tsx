018f3137ee36a6421383061646c8571f17e342be
IJssel <ijssel@synapt.nl>
2026-06-30 23:30:28 +0200

    Started on nav

--- app/ui/src/components/layout/sidebar/Sidebar.tsx
+++ app/ui/src/components/layout/sidebar/Sidebar.tsx
@@ -1,6 +1,32 @@
 import type { JSX } from "react/jsx-runtime";
 import { DeviceType } from "../PageLayout";
 import ContentSection from "../content/ContentSection";
+import SidebarItem from "./SidebarItem";
+import SidebarSection from "./SidebarSection";
+
+/* 
+Home
+views
+Puppets >
+- per puppet
+settings >
+- plugins
+- config
+*/
+function Nav({collapsed}: {collapsed: boolean}) {
+  return (
+    <nav>
+      <SidebarItem /> 
+      <SidebarItem /> 
+      <SidebarSection />
+      <SidebarItem />
+      <SidebarItem />
+      <SidebarSection />
+      <SidebarItem />
+      <SidebarItem />
+    </nav>
+  );
+}
 
 export default function Sidebar({isCollapsed, setCollapsed, deviceType, heightGrow = true, className}: {isCollapsed: boolean, setCollapsed: (state: boolean) => void, deviceType: DeviceType, heightGrow?: boolean, className?: string }): JSX.Element {
   return(
