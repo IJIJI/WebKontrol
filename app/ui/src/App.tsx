f6915aefe6c2c07c97dd0efb9207bf3798a6a76c
IJssel <ijssel@synapt.nl>
2026-06-30 15:29:17 +0200

    Started on router

--- app/ui/src/App.tsx
+++ app/ui/src/App.tsx
@@ -2,23 +2,25 @@ import type { JSX } from "react";
 import { AppErrorBoundary } from "./boundaries/AppErrorBoundary";
 import { ApiStateProvider } from "./context/ApiStateContext";
 import { BrandLogo } from "./components/branding/BrandLogo"
+import PageLayout from "./components/layout/PageLayout";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 export default function App(): JSX.Element {
   return (
     //TODO: Check if this is the right split. Different error boundary for the views?
     // TODO: error boundary inside or outside of router? What about state providers?
-    <>
-      <AppErrorBoundary>
-        <ApiStateProvider>
-          {/* <h1>HI</h1> */}
-          <BrandLogo version="V1.0.0" />
-        </ApiStateProvider>
-      </AppErrorBoundary>
-      {/* <AppErrorBoundary>
-        <ViewStateProvider>
-          <h1>Screen!</h1>
-        </ViewStateProvider>
-      </AppErrorBoundary> */}
-    </>
+    <AppErrorBoundary>
+    <ApiStateProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<PageLayout />}>
+            <Route index element={<h1>Test!</h1>} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+      {/* <h1>HI</h1> */}
+      {/* <BrandLogo version="V1.0.0" /> */}
+    </ApiStateProvider>
+    </AppErrorBoundary>
   );
 }
