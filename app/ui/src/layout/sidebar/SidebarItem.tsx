2486d3b4ac3ef59ea4373f8b8112a14b77f455ee
IJssel <ijssel@synapt.nl>
2026-06-30 14:07:33 +0200

    Added more nav types

---
+++ app/ui/src/layout/sidebar/SidebarItem.tsx
@@ -0,0 +1,5 @@
+import { JSX } from "react/jsx-runtime";
+
+export default function SidebarItem(): JSX.Element {
+  return(<></>);
+}
---
+++ app/ui/src/layout/sidebar/SidebarSection.tsx
@@ -0,0 +1,5 @@
+import { JSX } from "react/jsx-runtime";
+
+export default function SidebarSection(): JSX.Element {
+  return(<></>);
+}
