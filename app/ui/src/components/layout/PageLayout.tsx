34f38a597c8bee8ff6a7bd1fc0a09d042578d6f5
IJssel <ijssel@synapt.nl>
2026-06-30 23:26:06 +0200

    Added test checkbox

--- app/ui/src/components/layout/PageLayout.tsx
+++ app/ui/src/components/layout/PageLayout.tsx
@@ -6,6 +6,7 @@ import AmbientGlowBackground from "../background/AmbientGlowBackground"
 
 import "./layout.less";
 import MainContentContainer from "./content/MainContentContainer"
+import ContentSection from "./content/ContentSection"
 
 const _PAGE_TITLES: Record<string, string> = { // TODO: Remove in favour of page helper component? What about paths with dynamic params?
   '/overview':        'Home',
@@ -64,9 +65,12 @@ export default function PageLayout(): JSX.Element {
     <div className="page-base">
       <AmbientGlowBackground />
       <section className="page-layout">
-        <DesktopHeader className="page-header" version="v1.0.0" setCollapsed={setIsCollapsed}/>
+        <DesktopHeader className="page-header" version="v1.0" collapsed={isCollapsed} setCollapsed={setIsCollapsed}/>
         <Sidebar className="page-sidebar" isCollapsed={isCollapsed} setCollapsed={setIsCollapsed} deviceType={deviceType} />
         <MainContentContainer className="page-content">
+          <ContentSection>
+          <input type="checkbox" name="" id="" onClick={() => setIsCollapsed(!isCollapsed)} checked={isCollapsed}/>
+          </ContentSection>
           <Outlet />
         </MainContentContainer>
       </section>
