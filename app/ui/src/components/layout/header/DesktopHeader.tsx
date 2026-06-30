3116c83c90af7c1383dee9248db96e906dd2971a
IJssel <ijssel@synapt.nl>
2026-06-30 23:26:18 +0200

    removed default collapsed

--- app/ui/src/components/layout/header/DesktopHeader.tsx
+++ app/ui/src/components/layout/header/DesktopHeader.tsx
@@ -4,7 +4,7 @@ import { BrandLogo } from "../../branding/BrandLogo";
 import "./header.less";
 import ContentSection from "../content/ContentSection";
 
-export default function DesktopHeader({version, collapsed = false, setCollapsed, className}: {version: string, collapsed?: boolean, setCollapsed: (state: boolean) => void, className?: string }): JSX.Element {
+export default function DesktopHeader({version, collapsed, setCollapsed, className}: {version: string, collapsed: boolean, setCollapsed: (state: boolean) => void, className?: string }): JSX.Element {
   // TODO logo a link and autocollapse.
   return(
     <ContentSection variant="glass" className={["header", "desktop", "pad-m", className].filter(Boolean).join(" ")} >
