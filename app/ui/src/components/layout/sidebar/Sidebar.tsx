e157099ef6dd3953781c2dd2e370ca8895693467
IJssel <ijssel@synapt.nl>
2026-06-30 19:22:29 +0200

    Layout grid

--- app/ui/src/components/layout/sidebar/Sidebar.tsx
+++ app/ui/src/components/layout/sidebar/Sidebar.tsx
@@ -2,9 +2,9 @@ import type { JSX } from "react/jsx-runtime";
 import { DeviceType } from "../PageLayout";
 import ContentSection from "../content/ContentSection";
 
-export default function Sidebar({isCollapsed, setCollapsed, deviceType, className}: {isCollapsed: boolean, setCollapsed: (state: boolean) => void, deviceType: DeviceType, className?: string }): JSX.Element {
+export default function Sidebar({isCollapsed, setCollapsed, deviceType, heightGrow = true, className}: {isCollapsed: boolean, setCollapsed: (state: boolean) => void, deviceType: DeviceType, heightGrow?: boolean, className?: string }): JSX.Element {
   return(
-    <ContentSection variant="glass" className={["sidebar", deviceType == DeviceType.MOBILE && "mobile", "pad-none", className].filter(Boolean).join(" ")} >
+    <ContentSection variant="glass" className={["sidebar", deviceType == DeviceType.MOBILE && "mobile", "pad-none", heightGrow && "height-100", className].filter(Boolean).join(" ")} >
       <h1>test</h1>
     </ContentSection>
     
