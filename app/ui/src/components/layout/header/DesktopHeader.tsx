155b88d13a92ba277362a3b2b534449c5d08a558
IJssel <ijssel@synapt.nl>
2026-07-02 17:51:38 +0200

    Tweaked header

--- app/ui/src/components/layout/header/DesktopHeader.tsx
+++ app/ui/src/components/layout/header/DesktopHeader.tsx
@@ -7,8 +7,8 @@ import ContentSection from "../content/ContentSection";
 export default function DesktopHeader({version, collapsed, setCollapsed, className}: {version: string, collapsed: boolean, setCollapsed: (state: boolean) => void, className?: string }): JSX.Element {
   // TODO logo a link and autocollapse.
   return(
-    <ContentSection variant="glass" className={["header", "desktop", "pad-s", className].filter(Boolean).join(" ")} >
-      <BrandLogo size={22} version={version} collapsed={collapsed} /> 
+    <ContentSection variant="glass" className={["header", "desktop", "pad-xs", className].filter(Boolean).join(" ")} >
+      <BrandLogo size={20} version={version} collapsed={collapsed} /> 
     </ContentSection>
     
   );
