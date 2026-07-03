a33653285f929b6fda6ebe5a98b51ff4c1a95b6c
IJssel <ijssel@synapt.nl>
2026-07-04 00:17:59 +0200

    Added settings to page

--- app/ui/src/pages/SettingsPage.tsx
+++ app/ui/src/pages/SettingsPage.tsx
@@ -1,9 +1,36 @@
+import { useState } from "react";
 import ContentSection from "../components/layout/content/ContentSection";
+import { SettingGroup } from "../components/settings/SettingGroup";
+import { TextSetting } from "../components/settings/TextSetting";
+import { ButtonSelectSetting } from "../components/settings/ButtonSelectSetting";
+import { ToggleSetting } from "../components/settings/ToggleSetting";
 
+type placeHolderTheme = "light" | "dark" | "auto";
 
 export default function SettingsPage() {
 
+  const [theme, setTheme] = useState<placeHolderTheme>("auto");
+  const [disableBackground, setDisableBackground] = useState<boolean>(false);
+
   return (
-    <h1>Settings!</h1>
+    <>
+      <SettingGroup title="Appearance" >
+        <ButtonSelectSetting 
+          title="Theme" 
+          subtitle="Override your system color scheme" 
+          value={theme} setValue={setTheme} 
+          options={[
+            {label: "Auto", value: "auto"},
+            {label: "Light", value: "light"},
+            {label: "Dark", value: "dark"},
+          ]}
+        />
+        <ToggleSetting 
+          title="Theme" 
+          subtitle="Override your system color scheme" 
+          value={disableBackground} setValue={setDisableBackground} 
+        />
+      </SettingGroup>
+    </>
   );
 }
