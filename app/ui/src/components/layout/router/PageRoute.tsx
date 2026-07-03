27baf91d8892aad16d499869222a80ccb8d0489a
IJssel <ijssel@synapt.nl>
2026-07-03 21:23:00 +0200

    added back to pageroute

--- app/ui/src/components/layout/router/PageRoute.tsx
+++ app/ui/src/components/layout/router/PageRoute.tsx
@@ -3,11 +3,11 @@ import { type PageMeta, usePageContext } from "../../../context/PageContext";
 
 // TODO: Move somewhere else, does not belong in layout
 
-export const PageRoute = ({ children, title, description }: PageMeta & { children: ReactNode }): ReactNode => {
+export const PageRoute = ({ children, title, description, back }: PageMeta & { children: ReactNode }): ReactNode => {
   const { setMeta } = usePageContext();
 
   useEffect(() => {
-    setMeta({ title, description });
+    setMeta({ title, description, back });
   }, [title, description, setMeta]);
 
   return children;
