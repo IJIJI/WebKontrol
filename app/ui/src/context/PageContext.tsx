fade453d42c2bd5c9358f2d5f6813d17f3269548
IJssel <ijssel@synapt.nl>
2026-07-03 15:48:50 +0200

    Added desciption and todo

--- app/ui/src/context/PageContext.tsx
+++ app/ui/src/context/PageContext.tsx
@@ -1,18 +1,22 @@
 import { createContext, type JSX, useContext, useEffect, useState } from "react";
 
-interface PageState {
-  title?: string
+interface PageState { // TODO: Add more like og
+  title?: string;
+  description?: string;
 }
 
 const PageStateContext = createContext<PageState | null>(null);
 
+// TODO: Reset title and description on page load, check if a context is the best way to do this.
 export function PageStateProvider({
   children,
 }: {
   children: JSX.Element;
 }): JSX.Element {
 
-  const [state, setState] = useState<PageState>({});
+  const [state, setState] = useState<PageState>({
+    description: "WebKontrol remote browser - An intuitive web kiosk with a web-based admin panel."
+  });
 
   useEffect(() => {
     document.title = state.title ? state.title + "- WebKontrol" : "WebKontrol";
