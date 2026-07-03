ca0262d180738e847b649de67d91370e2d3bb8d7
IJssel <ijssel@synapt.nl>
2026-07-03 22:09:40 +0200

    Added back label test

--- app/ui/src/pages/OverviewPage.tsx
+++ app/ui/src/pages/OverviewPage.tsx
@@ -1,3 +1,4 @@
+import { NavLink } from "react-router-dom";
 import ContentSection from "../components/layout/content/ContentSection";
 
 
@@ -5,6 +6,9 @@ export default function OverviewPage() {
 
   return (
     <ContentSection>
+      <NavLink to={"/puppets/1"} state={{ back: {path: "/", label: "Home"}}}>
+        Puppet 1
+      </NavLink>
       <h1>Test!</h1>
       <h1>Test!</h1>
       <h1>Test!</h1>
