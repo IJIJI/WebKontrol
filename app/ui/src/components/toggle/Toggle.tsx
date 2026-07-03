b06ee67ccab71df1b309688191ecca0e06772190
IJssel <ijssel@synapt.nl>
2026-07-04 00:42:58 +0200

    Added ref to toggle

--- app/ui/src/components/toggle/Toggle.tsx
+++ app/ui/src/components/toggle/Toggle.tsx
@@ -1,13 +1,14 @@
 import { JSX } from 'react/jsx-runtime';
 import './toggle.less';
 
-export function Toggle({checked, setChecked, disabled}: {checked: boolean, setChecked: (value: boolean) => void, disabled?: boolean}): JSX.Element {
+export function Toggle({checked, setChecked, disabled, ref}: {checked: boolean, setChecked: (value: boolean) => void, disabled?: boolean, ref?: React.RefObject<HTMLDivElement | null>}): JSX.Element {
   return (
     <div 
       className={"toggle"+(disabled ? " disabled" : "")+(checked ? " checked" : "")}
       role="switch"
       aria-checked={checked}
       onClick={() => !disabled && setChecked(!checked)}
+      ref={ref}
     >
       <span className="knob"></span>
     </div>
