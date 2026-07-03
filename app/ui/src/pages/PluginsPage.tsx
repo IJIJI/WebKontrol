f7139063859344875d7207db94ccb5058327fbed
IJssel <ijssel@synapt.nl>
2026-07-03 18:04:37 +0200

    cleanup old page title code

--- app/ui/src/pages/PluginsPage.tsx
+++ app/ui/src/pages/PluginsPage.tsx
@@ -1,19 +1,9 @@
-import { useEffect } from "react";
 import ContentSection from "../components/layout/content/ContentSection";
-import { usePageContext } from "../context/PageContext";
-
 
 export default function PluginsPage() {
-
-  const { title, setTitle } = usePageContext();
-
-  useEffect(() => {
-    setTitle("Plugins?");
-  }, []);
-
   return (
     <ContentSection>
-      <h1>Plugins! - {title}</h1>
+      <h1>Plugins!</h1>
     </ContentSection>
   );
 }
