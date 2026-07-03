a81e5e13b134146694f85d3528051dad5ab394ff
IJssel <ijssel@synapt.nl>
2026-07-03 18:28:46 +0200

    Implemented pageRoute helper

--- app/ui/src/App.tsx
+++ app/ui/src/App.tsx
@@ -9,6 +9,7 @@ import ViewsPage from "./pages/ViewsPage";
 import SettingsPage from "./pages/SettingsPage";
 import PluginsPage from "./pages/PluginsPage";
 import { PageStateProvider } from "./context/PageContext";
+import { PageRoute } from "./components/layout/router/PageRoute";
 
 export default function App(): JSX.Element {
   return (
@@ -20,12 +21,12 @@ export default function App(): JSX.Element {
         <BrowserRouter>
           <Routes>
             <Route element={<PageLayout />}>
-              <Route index element={<OverviewPage />} />
-              <Route path="dashboard" element={<DashboardPage />} />
-              <Route path="views" element={<ViewsPage />} />
-              
-              <Route path="settings/plugins" element={<PluginsPage />} />
-              <Route path="settings/config" element={<SettingsPage />} />
+              <Route index element={<PageRoute><OverviewPage /></PageRoute>} />
+              <Route path="dashboard" element={<PageRoute title="Dashboard"><DashboardPage /></PageRoute>} />
+              <Route path="views" element={<PageRoute title="Views"><ViewsPage /></PageRoute>} />
+
+              <Route path="settings/plugins" element={<PageRoute title="Plugins"><PluginsPage /></PageRoute>} />
+              <Route path="settings/config" element={<PageRoute title="Settings"><SettingsPage /></PageRoute>} />
 
               <Route path="*"  element={<Navigate to="/" replace />} />
             </Route>
