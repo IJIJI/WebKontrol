534d5b52e01805bc6f83fbfb65638dc07438d1c4
IJssel <ijssel@synapt.nl>
2026-07-03 21:19:27 +0200

    Added WithRequiredExept

--- app/ui/src/context/PageContext.tsx
+++ app/ui/src/context/PageContext.tsx
@@ -1,20 +1,25 @@
 import { createContext, type ReactNode, useCallback, useContext, useEffect, useState } from "react";
 import { type WithRequired } from "../../../src/types/CommonTypes";
 
+export interface BackConfig {
+  path: string;
+  label?: string;
+};
+
 export interface PageMeta { // TODO: Add more like og
   title?: string;
   description?: string;
+  back?: BackConfig | false;
 }
 
+type RequiredPageMeta = Required<PageMeta>;
 type RequiredPageMetaInput = WithRequired<PageMeta, "description">;
-type RequiredPageMetaDefaults = Required<Omit<PageMeta, "description">>;
 
-export const DEFAULT_PAGE_META: RequiredPageMetaInput = {
+
+const DEFAULT_PAGE_META: RequiredPageMeta = {
+  title: "WebKontrol",
   description: "WebKontrol remote browser - An intuitive web kiosk with a web-based admin panel.",
-}
-
-const FALLBACK_PAGE_META: RequiredPageMetaDefaults = {
-  title: "WebKontrol"
+  back: false,
 }
 
 interface PageState extends Required<PageMeta> {
@@ -29,7 +34,7 @@ export function PageStateProvider({
   children: ReactNode;
 }): ReactNode {
 
-  const [meta, setMetaState] = useState<RequiredPageMetaInput>(DEFAULT_PAGE_META);
+  const [meta, setMetaState] = useState<RequiredPageMeta>(DEFAULT_PAGE_META);
 
   const setMeta = useCallback((next: PageMeta, keepPrevious?: boolean) => {
     setMetaState((prev) => {
@@ -61,6 +66,7 @@ export function PageStateProvider({
         ...meta,
         // title: meta.title ?? FALLBACK_PAGE_META.title,
         title: meta.title ?? "",
+
         setMeta,
       }}
     >
