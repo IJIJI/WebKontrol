f3cb3cc9bb8f8907485674d40c02e0a3e0dcbe87
IJssel <ijssel@synapt.nl>
2026-07-03 23:57:29 +0200

    Added text class

--- app/ui/src/components/settings/TextSetting.tsx
+++ app/ui/src/components/settings/TextSetting.tsx
@@ -11,7 +11,7 @@ export function TextSetting({title, subtitle, value, setValue, disabled}: {title
         <span className="title">{title}</span>
         <span className="subtitle">{subtitle}</span>
       </div>
-      <div className="input">
+      <div className="input text">
         <input type="text" ref={inputRef} value={value} onChange={(event) => setValue(event.target.value)} disabled={disabled} />
       </div>
     </div>
