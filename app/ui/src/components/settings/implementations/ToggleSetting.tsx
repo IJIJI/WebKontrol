e1f439d0c358e56bbe6831da5168a37365a0abb1
IJssel <ijssel@synapt.nl>
2026-07-04 00:43:35 +0200

    converted toggle to new base

--- app/ui/src/components/settings/implementations/ToggleSetting.tsx
+++ app/ui/src/components/settings/implementations/ToggleSetting.tsx
@@ -3,19 +3,16 @@ import { Toggle } from "../../toggle/Toggle";
 
 import "../settings.less";
 
+import { BaseSetting } from "../BaseSetting";
+import { useRef } from "react";
 
 export function ToggleSetting({title, subtitle, value, setValue, disabled}: {title: string, subtitle: string, value: boolean, setValue: (value: boolean) => void | Promise<void>, disabled?: boolean}): JSX.Element {
+  const inputRef = useRef<HTMLDivElement>(null);
 
   return (
-    <div className="setting field">
-      <div className="title">
-        <span className="title">{title}</span>
-        <span className="subtitle">{subtitle}</span>
-      </div>
-      <div className="input">
-        <Toggle checked={value} setChecked={setValue} disabled={disabled} />
-      </div>
-    </div>
+    <BaseSetting inputRef={inputRef} title={title} subtitle={subtitle}>
+        <Toggle ref={inputRef} checked={value} setChecked={setValue} disabled={disabled} />
+    </BaseSetting>
   );
 
 }
