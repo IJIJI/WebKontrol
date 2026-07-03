e961cd09ab4d1343ab8c4abdcccc3b4a4802658d
IJssel <ijssel@synapt.nl>
2026-07-03 23:54:44 +0200

    Started on TextSetting

---
+++ app/ui/src/components/settings/TextSetting.tsx
@@ -0,0 +1,20 @@
+import { useRef } from "react";
+import { JSX } from "react/jsx-runtime";
+
+
+export function TextSetting({title, subtitle, value, setValue, disabled}: {title: string, subtitle: string, value: string, setValue: (value: string) => RTCEncodedVideoFrameMetadata, disabled?: boolean}): JSX.Element {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  return (
+    <div className="setting field" onClick={() => inputRef.current?.focus()}>
+      <div className="title">
+        <span className="title">{title}</span>
+        <span className="subtitle">{subtitle}</span>
+      </div>
+      <div className="input">
+        <input type="text" ref={inputRef} value={value} />
+      </div>
+    </div>
+  );
+
+}
