2c4668dd62d871bc3a7e45f40699f6add10a916e
IJssel <ijssel@synapt.nl>
2026-07-05 01:20:09 +0200

    Also added resture to buttonselect and toggle

--- app/ui/src/components/settings/implementations/ButtonSelectSetting.tsx
+++ app/ui/src/components/settings/implementations/ButtonSelectSetting.tsx
@@ -3,6 +3,7 @@ import { type JSX } from "react/jsx-runtime";
 import "../settings.less";
 
 import { BaseSetting, type BaseSettingProps } from "../BaseSetting";
+import { RestoreButton } from "../RestoreButton";
 
 type ButtonSelectProps<OptionT> = BaseSettingProps<OptionT> & {
   options: { label: string; value: OptionT }[];
@@ -12,14 +13,21 @@ export function ButtonSelectSetting<T>(
   props: ButtonSelectProps<T>,
 ): JSX.Element {
 
+  const changed = props.savedVal !== undefined && props.savedVal !== props.value;
+  const restore = (): void => {
+    if (props.savedVal === undefined) return;
+    props.setValue(props.savedVal);
+  };
+
   return (
-    <BaseSetting {...props} >
+    <BaseSetting {...props} changed={changed} >
+      {changed ? <RestoreButton onClick={restore} /> : <></>}
       <div className="buttonSelect">
         {props.options.map((option, index) => (
           <button
             key={option.label}
             type="button"
-            className={option.value === props.value ? "active" : ""}
+            className={option.value === props.value ? "active" : "" + option.value === props.savedVal ? " previous" : ""}
             disabled={props.disabled}
             onClick={() => {
               props.setValue(option.value);
--- app/ui/src/components/settings/implementations/ToggleSetting.tsx
+++ app/ui/src/components/settings/implementations/ToggleSetting.tsx
@@ -5,14 +5,22 @@ import "../settings.less";
 
 import { BaseSetting, type BaseSettingProps } from "../BaseSetting";
 import { useRef } from "react";
+import { RestoreButton } from "../RestoreButton";
 
 type ToggleProps = BaseSettingProps<boolean>;
 
 export function ToggleSetting(props: ToggleProps): JSX.Element {
   const inputRef = useRef<HTMLDivElement>(null);
 
+  const changed = props.savedVal !== undefined && props.savedVal !== props.value;
+  const restore = (): void => { // TODO: Does this need a restore button?
+    if (props.savedVal === undefined) return;
+    props.setValue(props.savedVal);
+  };
+
   return (
-    <BaseSetting {...props} inputRef={inputRef}>
+    <BaseSetting {...props} changed={changed} inputRef={inputRef}>
+      {changed ? <RestoreButton onClick={restore} /> : <></>}
       <Toggle
         ref={inputRef}
         className={"toggleField"}
