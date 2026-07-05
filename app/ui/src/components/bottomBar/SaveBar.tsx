b2952dd18812c589b5fa8f8660ee96a4458bcf30
IJssel <ijssel@synapt.nl>
2026-07-05 13:15:45 +0200

    SaveBar promise handeling

--- app/ui/src/components/bottomBar/SaveBar.tsx
+++ app/ui/src/components/bottomBar/SaveBar.tsx
@@ -4,6 +4,7 @@ import "./bar.less";
 import { BottomBar } from "./BottomBar";
 import { Button, ButtonType } from "../button/Button";
 import { Icons } from "../icons/Icons";
+import { useState } from "react";
 
 export function SaveBar({
   visible,
@@ -14,16 +15,25 @@ export function SaveBar({
   onSave: () => void | Promise<void>,
   onDiscard: () => void | Promise<void>,
 }): JSX.Element {
+
+  const [isSaving, setIsSaving] = useState<boolean>(false);
+
+  const doSave = async (): Promise<void> => {
+    setIsSaving(true);
+    await onSave();
+    setIsSaving(false);
+  }
+
   return (
     <BottomBar visible={visible} className="saveBar">
       <span>Unsaved changes</span>
       <div className="saveButtons">
-        <Button type={ButtonType.DANGER} onClick={onDiscard} > 
+        <Button type={ButtonType.DANGER} onClick={onDiscard} disabled={isSaving}> 
           <Icons.undo size={20} />
           <p>Discard</p>
         </Button>
-        <Button type={ButtonType.SUCCESS} onClick={onSave} > 
-          <Icons.check size={19} />
+        <Button type={ButtonType.SUCCESS} onClick={doSave} > 
+          { isSaving ? <Icons.loading size={19}/> : <Icons.check size={19} /> }
           <p>Save</p>
         </Button>
       </div>
