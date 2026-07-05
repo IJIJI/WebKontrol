43f830e771ece6ace20cab6489be36e4d58e9e8e
IJssel <ijssel@synapt.nl>
2026-07-05 16:24:06 +0200

    Made statuspill collapsible

--- app/ui/src/components/pill/statusPill/StatusPill.tsx
+++ app/ui/src/components/pill/statusPill/StatusPill.tsx
@@ -18,13 +18,14 @@ export function StatusPill(props: {
   label?: string;
   status: ConnectionState;
   style?: PillStyle;
+  collapsed?: boolean
 }): JSX.Element {
   const state = STATE_MAP[props.status];
   const style = props.style ? props.style : state.style;
   const label = props.label ? props.label : state.label;
 
   return (
-    <InfoPill className="statusPill" style={style} type={state.type} size={props.size}>
+    <InfoPill className={"statusPill" + (props.collapsed ? " collapsed" : "")} style={style} type={state.type} size={props.size}>
       <span className="dotContainter">
         <div className="dot"></div>
       </span> 
