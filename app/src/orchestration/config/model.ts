2278639f4fed33898f3ccf8204a9a8e5865d8f6e
IJssel <ijssel@synapt.nl>
2026-07-05 22:58:35 +0200

    Updated AppConfig

--- app/src/orchestration/config/model.ts
+++ app/src/orchestration/config/model.ts
@@ -1,10 +1,17 @@
 import z from "zod";
 import { ConfigFilePuppetSpecificSchema } from "../../puppet/validation";
+import { PuppetGlobalConfigSchema } from "../../puppet/schema";
+import { WebServerConfigSchema } from "../../webServer/schema";
 
 
 export const AppConfigSchema = z.object({
-  puppets: z.array(ConfigFilePuppetSpecificSchema),
+  puppets: z.object({
+    global: PuppetGlobalConfigSchema,
+    entries: z.array(ConfigFilePuppetSpecificSchema),
+  }),
+  web: WebServerConfigSchema.default(WebServerConfigSchema.parse({}))
 });
+
 
 export type AppConfig = z.infer<typeof AppConfigSchema>;
 
