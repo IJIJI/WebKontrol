3af7c5f8420fecf77679e95b10023c5c8ba00f7d
IJssel <ijssel@synapt.nl>
2026-07-05 21:11:50 +0200

    Added ConfigManager

--- app/src/orchestration/config/ConfigManager.ts
+++ app/src/orchestration/config/ConfigManager.ts
@@ -1,5 +1,8 @@
+import fs from "node:fs";
 import path from "node:path";
-
+import { AppConfigSchema, type AppConfig } from "./model";
+import YAML from "yaml";
+import { Logger } from "../../logging/Logger";
 
 export class ConfigManager {
   private static readonly CONFIG_PATH = path.join(
@@ -8,5 +11,37 @@ export class ConfigManager {
     `config.yaml`,
   );  
 
+  protected _logger: Logger;
+  protected _config!: AppConfig;
   
+  constructor() {
+    this._logger = new Logger(["LifeCycle","CONFIG","LOADER"]);
+  }
+
+  private get _isInit(): boolean {
+    return this._config !== undefined;
+  }
+
+  public async init(): Promise<void> {
+    const path = ConfigManager.CONFIG_PATH; // TODO: Env override?
+
+    try {
+      const file = fs.readFileSync(path, 'utf8');
+      const data = YAML.parse(file);
+
+      const parsed = AppConfigSchema.parse(data);
+      this._config = parsed;
+    } catch(error) {
+      return this._logger.fatal("Failed loading config from file. Path:", path, "Error", error);
+    }
+  }
+
+  public getConfig(): AppConfig {
+    if (!this._isInit)
+      return this._logger.fatal("Tried getting the appconfig, but it has not been loaded yet!");
+
+    return this._config;
+  }
+
+
 }
