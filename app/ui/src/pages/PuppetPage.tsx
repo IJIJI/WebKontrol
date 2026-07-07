7cbcf818a907e8d240bbfc8eed2d0a2e083fd41e
IJssel <ijssel@synapt.nl>
2026-07-07 23:43:22 +0200

    Started puppet specific page

--- app/ui/src/pages/PuppetPage.tsx
+++ app/ui/src/pages/PuppetPage.tsx
@@ -1,18 +1,29 @@
 import { useParams } from "react-router-dom";
 import { useApi } from "../context/ApiStateContext";
 import { usePageContext } from "../context/PageContext";
-import { type JSX, useEffect } from "react";
+import { type JSX, useEffect, useState } from "react";
 
 export default function PuppetPage(): JSX.Element {
   const { puppetKey } = useParams();
   const { state } = useApi();
   const { setMeta } = usePageContext();
+
+  const [title, setTitle] = useState<string>(puppetKey ?? "Unkown Puppet");
   const puppet = puppetKey ? state?.puppets.get(puppetKey) : undefined;
+  
+  useEffect(() => {
+    setTitle(puppet?.config.name.long ?? puppetKey ?? "Unkown Puppet");
+  }, [puppet])
 
   useEffect(() => {
     // if (puppet) setMeta({ title: ["Puppet", puppet.displayName] }, true);
-    setMeta({ title: ["Puppets", puppetKey ?? "Unkown"] }, true);
-  }, [puppetKey, setMeta]);
+    setMeta({ title: ["Puppets", title] }, true);
+  }, [puppetKey, setMeta, title]);
 
-  return <h1>Puppet specific page!</h1>;
+  return (
+    <>
+      <h1>{title}</h1>
+      <pre>{JSON.stringify(puppet, null, 2)}</pre>
+    </>
+  );
 }
