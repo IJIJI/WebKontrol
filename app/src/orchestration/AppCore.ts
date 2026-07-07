0e7c47f407fd8801879992f3ab36c08ad260081a
IJssel <ijssel@synapt.nl>
2026-07-07 19:45:47 +0200

    rewrote new appcore

--- app/src/orchestration/AppCore.ts
+++ app/src/orchestration/AppCore.ts
@@ -38,12 +38,52 @@ export class AppCore { // TODO: Move every non-puppet management from the appcor
     if(this._isInit){
       this._logger.warn("Init called on already initialised AppCore. Disregarding.");
     }
+    this._logger.important("Starting AppCore...");
 
     //TODO: Wire WebServer, ...
     await this._systemManager.init();
+
     await this._puppetOrchestrator.init();
+
     await this._uiManager.init();
+    
+    this._webServer.setHandlers({
+      system: this._systemManager.getHandlers(),
+      update: { // TODO: UpdateManager
+        check: async (): Promise<void> => {
+          this._logger.important(`update.check() handler called.`);
+        },
+        apply: async (
+          ref: string,
+          type: "release" | "branch",
+        ): Promise<void> => {
+          this._logger.important(
+            `update.apply() handler called with ref: ${ref} of type: ${type}`,
+          );
+        },
+        getStatus: async (): Promise<void> => {
+          this._logger.important(`update.getStatus() handler called.`);
+        },
+      },
+      puppet: this._puppetOrchestrator.getHandlers(),
+      ui: this._uiManager.getHandlers(),
+    });
     await this._webServer.start(); // TODO rename to init? Or split?
+  }
+
+  private async _sysncWebState(): Promise<void> {
+
+    this._logger.debug(`Syncing state to webserver...`);
+    this._webServer.setState({
+      puppets: await this._puppetOrchestrator.getPuppetInfoBundles(),
+      runtime: {
+        system: this._systemManager.getRuntime(),
+        ui: this._uiManager.getRuntime(),
+      },
+      info: {
+        system: this._systemManager.getInfo(),
+      }
+    });
   }
   
 
