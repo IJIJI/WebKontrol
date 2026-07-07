d5c07e89df12e6ca01938475d8a63aaf364046c9
IJssel <ijssel@synapt.nl>
2026-07-07 18:43:09 +0200

    CoreStore -> SystemStore

--- app/src/storage/SystemStore.ts
+++ app/src/storage/SystemStore.ts
@@ -1,31 +1,32 @@
 import { CoreRuntimeConfigSchema, type CoreRuntimeConfig } from "../core/schema";
 import { Logger } from "../logging/Logger";
+import type { SystemRuntime } from "../system/schema";
 import { CoreDatabase } from "./CoreDatabase";
 
-export class AppCoreStore {
+export class SystemStore {
   private _db = CoreDatabase.getInstance();
   private _logger: Logger;
 
   constructor() {
-    this._logger = new Logger(["STORE", "CORE"]);
+    this._logger = new Logger(["STORE", "SYSTEM"]);
     this._logger.debug(`Constructed store.`);
   }
 
-  public async saveRuntime(config: CoreRuntimeConfig): Promise<void> {
+  public async saveRuntime(config: SystemRuntime): Promise<void> {
     this._logger.debug(`Saving runtime...`);
     await this._db.updateSetting(
       "app",
-      "core",
+      "system",
       "runtime",
       JSON.stringify(config),
     );
     this._logger.debug(`Successfully saved runtime!`);
   }
 
-  public async loadRuntime(): Promise<CoreRuntimeConfig | null> {
+  public async loadRuntime(): Promise<SystemRuntime | null> {
     try {
       this._logger.debug(`Loading runtime...`);
-      const raw = await this._db.getSetting("app", "core", "runtime");
+      const raw = await this._db.getSetting("app", "system", "runtime");
       if (raw === null) {
         this._logger.info(`Failed loading runtime! Got null`);
         return null;
