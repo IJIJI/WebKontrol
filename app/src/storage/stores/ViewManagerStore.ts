5c77ccde82740413994f12c1f42bac6b2625e5d9
IJssel <ijssel@synapt.nl>
2026-07-08 16:48:59 +0200

    Updated viewManager store

--- app/src/storage/stores/ViewManagerStore.ts
+++ app/src/storage/stores/ViewManagerStore.ts
@@ -1,9 +1,10 @@
 import { Logger } from "../../logging/Logger";
+import { ViewManagerRuntimeSchema, type ViewManagerRuntime } from "../../views/types/schema";
 import type {
   AbstractView,
   ViewConfig,
   ViewKey,
-} from "../../views/types/AbstractView";
+} from "../../views/views/AbstractView";
 import { CoreDatabase } from "../CoreDatabase";
 
 export class ViewManagerStore {
@@ -14,37 +15,35 @@ export class ViewManagerStore {
     this._logger = new Logger(["STORE", "VIEWMANAGER"]);
     this._logger.debug(`Constructed store.`);
   }
+  
+  public async saveRuntime(config: ViewManagerRuntime): Promise<void> {
+    this._logger.debug(`Saving runtime...`);
+    await this._db.updateSetting(
+      "app",
+      "system",
+      "runtime",
+      JSON.stringify(config),
+    );
+    this._logger.debug(`Successfully saved runtime!`);
+  }
 
-  // public async saveRuntime(config: SystemConfig): Promise<void> {
-  //   this._logger.debug(`Saving runtime...`);
-  //   await this._db.updateSetting(
-  //     "app",
-  //     "viewmanager",
-  //     "runtime",
-  //     JSON.stringify(config),
-  //   );
-  //   this._logger.debug(`Successfully saved runtime!`);
-  // }
+  public async loadRuntime(): Promise<ViewManagerRuntime | null> {
+    try {
+      this._logger.debug(`Loading runtime...`);
+      const raw = await this._db.getSetting("app", "system", "runtime");
+      if (raw === null) {
+        this._logger.info(`Failed loading runtime! Got null`);
+        return null;
+      }
+      const object = ViewManagerRuntimeSchema.parse(JSON.parse(raw));
+      this._logger.debug(`Successfully loaded runtime!`, object);
+      return object;
+    } catch (error) {
+      this._logger.error(`Failed loading runtime with error:`, error);
+      return null;
+    }
+  }
 
-  // public async loadRuntime(): Promise<SystemConfig | null> {
-  //   try {
-  //     this._logger.debug(`Loading runtime...`);
-  //     const raw = await this._db.getSetting("app", "viewmanager", "runtime");
-  //     if (raw === null) {
-  //       this._logger.info(`Failed loading runtime! Got null`);
-  //       return null;
-  //     }
-  //     const object = SystemConfigSchema.parse(JSON.parse(raw));
-  //     this._logger.debug(`Successfully loaded runtime!`, object);
-  //     return object;
-  //   } catch (error) {
-  //     this._logger.error(
-  //       `Failed loading runtime with error:`,
-  //       error,
-  //     );
-  //     return null;
-  //   }
-  // }
 
   public async getViews(): Promise<Map<ViewKey, AbstractView>> {
     this._logger.debug(`Loading views...`);
