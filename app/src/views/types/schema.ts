8ee124efdc15f8f0c3946c4f20c4a19dd71bee4c
IJssel <ijssel@synapt.nl>
2026-07-08 16:52:40 +0200

    Added runtime field

--- app/src/views/types/schema.ts
+++ app/src/views/types/schema.ts
@@ -1,11 +1,13 @@
 import z from "zod";
+import { LoadTimeoutSchema, LoadTimeoutSchemaDefault } from "../../puppet/types/schema";
 
 
 export const ViewManagerRuntimeShape = z.object({
- // TODO
+  default_load_timeout: LoadTimeoutSchema, // TODO: Add usefull fields, this is a placeholder.
 });
 
 export const ViewManagerRuntimeSchema = ViewManagerRuntimeShape.extend({
+  default_load_timeout: LoadTimeoutSchemaDefault
 });
 
 export type ViewManagerRuntime = z.infer<typeof ViewManagerRuntimeSchema>;
