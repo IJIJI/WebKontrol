b119c42e4dc887ec9867e45f50e4382258b8b755
IJssel <ijssel@synapt.nl>
2026-07-09 23:24:03 +0200

    Updated registry for new class blocks

--- app/src/views/blocks/registry.ts
+++ app/src/views/blocks/registry.ts
@@ -1,21 +1,22 @@
-import { type BlockKey, type BlockTypeDefinition } from "./types/schema";
+import type { AbstractBlockType } from "./types/model";
+import { type BlockKey } from "./types/schema";
 
 
 export class BlockTypeRegistry {
-  private _types = new Map<BlockKey, BlockTypeDefinition>();
+  private _types = new Map<BlockKey, AbstractBlockType<any>>();
 
-  register(definition: BlockTypeDefinition): void {
+  register(definition: AbstractBlockType<any>): void {
     if (this._types.has(definition.key)) {
       throw new Error(`Block type with key: "${definition.key}" is already registered.`);
     }
     this._types.set(definition.key, definition);
   }
-  get(key: BlockKey): BlockTypeDefinition | undefined {
+  get(key: BlockKey): AbstractBlockType<any> | undefined {
     return this._types.get(key);
   }
-  list(): BlockTypeDefinition[] {
+  list(): AbstractBlockType<any>[] {
     return [...this._types.values()];
   }
 }
 
-export const blockTypeRegistry = new BlockTypeRegistry(); // TODO: Singleton instead?
+export const blockTypeRegistry = new BlockTypeRegistry();
