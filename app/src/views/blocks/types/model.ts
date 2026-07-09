bbe982943d0609ab836a5efe37b0991480315808
IJssel <ijssel@synapt.nl>
2026-07-09 02:23:21 +0200

    Started on abstract block type class

--- app/src/views/blocks/types/model.ts
+++ app/src/views/blocks/types/model.ts
@@ -1,7 +1,19 @@
-import type { BlockKey } from "./schema";
+import type z from "zod";
+import type { BlockKey, DataSourceKey } from "./schema";
 
 //* Resolved Blocks:
 export interface ResolvedBlock<TConfig = unknown> { // TODO: Move to registry types?
   type: BlockKey,
   config: TConfig,
+}
+
+//* Model Registration:
+export abstract class AbstractBlockType<TConfig = unknown> {
+  abstract readonly key: BlockKey;
+  abstract readonly configSchema: z.ZodType<TConfig>;
+  readonly fixedDataDependencies: DataSourceKey[] = [];
+
+  resolveDirect(config: TConfig): string | undefined { // TODO: Right return type? Right name?
+    return undefined;
+  }
 }
