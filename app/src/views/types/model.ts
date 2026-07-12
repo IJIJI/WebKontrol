268031b59a1abdbce19e8e5bb45ce7a8c7b9ac3b
IJssel <ijssel@synapt.nl>
2026-07-12 23:56:12 +0200

    Added ServeResult

--- app/src/views/types/model.ts
+++ app/src/views/types/model.ts
@@ -1,5 +1,16 @@
-
+import type { AnyBlockConfig } from "../blocks/types/schema";
 
 export interface ViewManagerInfo {
   viewCount: number; // TODO Actually usefull info
 }
+
+/**
+ * What a view's /view/:key path should return, produced by AbstractView.serve()
+ * and mapped to an HTTP response by the serving route:
+ *   - blocks   => render the root block tree (client-side)
+ *   - redirect => 302 the puppet to an external url
+ */
+// TODO: Check if the base path should be configurable somehow
+export type ServeResult =
+  | { kind: "blocks"; root: AnyBlockConfig }
+  | { kind: "redirect"; url: string };
