514477494ef3b89e3973be493b7f1e776316b7ae
IJssel <ijssel@synapt.nl>
2026-07-22 13:58:18 +0200

    added  collectionprops

--- app/ui/src/components/collections/types.ts
+++ app/ui/src/components/collections/types.ts
@@ -1,5 +1,21 @@
+import { ReactNode } from "react";
+
 export enum CollectionLayout {
   LIST = "list",
   GRID = "grid",
   TABLE = "table",
 } 
+
+
+export interface CollectionLayoutProps<T> {
+  items: T[];
+  getKey: (item: T) => string;
+  renderItem: (item: T) => ReactNode;
+}
+
+export interface CollectionProps<T> extends CollectionLayoutProps<T> {
+  layout?: CollectionLayout; // Defaults to list // TODO Add the option for different defaults? -> Not optional?
+  title?: ReactNode;
+  actions?: ReactNode;
+  empty?: ReactNode; // Content for when the list is empty.
+}
