21a5c2c1c0d6fec723e654a560ae1f0573d5615e
IJssel <ijssel@synapt.nl>
2026-07-24 15:50:59 +0200

    collections file start

---
+++ app/ui/src/components/collections/Collection.tsx
@@ -0,0 +1,39 @@
+
+
+
+
+import { JSX } from "react/jsx-runtime";
+import "./collection.less";
+import { CollectionLayout, CollectionProps } from "./types";
+import { ListLayout } from "./layouts/ListLayout";
+
+export function Collection<T>( props: CollectionProps<T> ): JSX.Element {
+  const baseClass = ["collection"];
+  
+  return (
+    <section className={[...baseClass].filter(Boolean).join(" ")} >
+      {(props.title || props.actions) && (
+        <header className={[...baseClass, "toolbar"].filter(Boolean).join(" ")} >
+          {props.title && <h2 className={[...baseClass, "title"].filter(Boolean).join(" ")} >{props.title}</h2>}
+          {props.actions && <div className={[...baseClass, "actions"].filter(Boolean).join(" ")} >{props.actions}</div>}
+        </header>
+      )}
+      {props.items.length === 0 ?
+        <div className={[...baseClass, "empty"].filter(Boolean).join(" ")} >
+          {props.empty ?? "Nothing here yet."}
+        </div> 
+        :
+        renderLayout(props)
+      }
+    </section>
+  );
+}
+
+function renderLayout<T>(props: CollectionProps<T>): JSX.Element {
+  switch (props.layout) {
+    case CollectionLayout.LIST:
+    default:
+      return <ListLayout {...props} />;
+    // TODO: More layouts
+  }
+}
