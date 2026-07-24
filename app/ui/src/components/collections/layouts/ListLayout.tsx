19d63cbd9043bc0b67a80066c461d7144acb3470
IJssel <ijssel@synapt.nl>
2026-07-24 15:30:24 +0200

    Added ListLayout.tsx

---
+++ app/ui/src/components/collections/layouts/ListLayout.tsx
@@ -0,0 +1,20 @@
+import { JSX } from "react/jsx-runtime";
+import { CollectionLayoutProps } from "../types";
+
+
+
+
+
+
+export function ListLayout<T>(props: CollectionLayoutProps<T>): JSX.Element {
+  const baseClass = ["collection", "layout", "list"];
+  return (
+    <div className={[...baseClass].filter(Boolean).join(" ")}>
+      {props.items.map((item) => (
+        <div key={props.getKey(item)} className={[...baseClass, "item"].filter(Boolean).join(" ")}>
+          {props.renderItem(item)}
+        </div>
+      ))}
+    </div>
+  );
+}
