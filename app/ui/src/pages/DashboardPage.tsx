7cf52dba3329eb892bf082d1597489253015648b
IJssel <ijssel@synapt.nl>
2026-07-25 12:01:11 +0200

    Added dashboard empty collection

--- app/ui/src/pages/DashboardPage.tsx
+++ app/ui/src/pages/DashboardPage.tsx
@@ -1,5 +1,20 @@
 import { type JSX } from "react/jsx-runtime";
+import { Collection } from "../components/collections/Collection";
+import { CollectionLayout } from "../components/collections/types";
+import { Button } from "../components/button/Button";
+import { ListItem } from "../components/collections/items/ListItem";
 
 export default function DashboardPage(): JSX.Element {
-  return <h1>Dashboard!</h1>;
+  return (
+    <Collection
+      items={[]}
+      getKey={(v) => "1"}
+      layout={CollectionLayout.LIST}
+      title="Dashboard"
+      actions={[]}
+      renderItem={(v) => (
+        <ListItem to={"/"} title={""} icon={""} color={"#FF0000"} actions={[]} />
+      )}
+    />
+  ); 
 }
