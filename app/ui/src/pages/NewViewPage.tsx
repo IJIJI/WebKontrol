a2ee6afb18236d328bb72fe231a3540b1e6ab6f0
IJssel <ijssel@synapt.nl>
2026-07-26 01:26:04 +0200

    Started newViewPage

--- app/ui/src/pages/NewViewPage.tsx
+++ app/ui/src/pages/NewViewPage.tsx
@@ -1,5 +1,35 @@
 import { type JSX } from "react/jsx-runtime";
+import { Button, ButtonType } from "../components/button/Button";
+import { SubmitEventHandler, useRef } from "react";
 
 export default function NewViewPage(): JSX.Element {
-  return <h1>New View!</h1>;
+
+  const formRef = useRef<HTMLFormElement>(null);
+
+  const handleForm = async (event: any): Promise<void> => {
+    event.preventDefault();
+
+    const datadiv = document.getElementById('datad');
+    if (datadiv) 
+      datadiv.innerHTML = JSON.stringify(event, null, 2);
+    console.log(JSON.stringify(event, null, 2));
+  }
+
+  return (
+    <>
+      <h1>New View</h1>
+      <form onSubmit={handleForm} ref={formRef}>
+        <div className="formRow">
+          <input type="text" name="name" id="viewName" />
+          <select name="type" id="viewType">
+            <option value="" disabled selected>-</option>
+            <option value="website">website</option>
+          </select>
+          <input type="url" name="website" id="viewUrl" />
+          <Button onClick={() => formRef.current?.requestSubmit()} type={ButtonType.ACCENT}>Create</Button>
+        </div>
+      </form>
+      <div id="datad"></div>
+    </>
+  );
 }
