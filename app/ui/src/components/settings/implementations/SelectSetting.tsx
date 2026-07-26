b091530fe32223eb507bc4ec5a1ea5ce7d3e888c
IJssel <ijssel@synapt.nl>
2026-07-26 23:07:16 +0200

    Added SelectSetting

---
+++ app/ui/src/components/settings/implementations/SelectSetting.tsx
@@ -0,0 +1,39 @@
+import { type JSX } from "react/jsx-runtime";
+
+import "../settings.less";
+import { BaseSetting, type BaseSettingProps } from "../BaseSetting";
+import { RestoreButton } from "../RestoreButton";
+
+type SelectProps<T extends string> = BaseSettingProps<T> & {
+  options: { label: string; value: T }[];
+};
+
+// A dropdown select. Preferred over ButtonSelectSetting when there are many options
+// (e.g. long enums). Value type is constrained to string since <option> values are strings.
+export function SelectSetting<T extends string>(props: SelectProps<T>): JSX.Element {
+  const changed = props.savedVal !== undefined && props.savedVal !== props.value;
+  const restore = (): void => {
+    if (props.savedVal === undefined) return;
+    props.setValue(props.savedVal);
+  };
+
+  return (
+    <BaseSetting {...props} changed={changed}>
+      {changed ? <RestoreButton onClick={restore} /> : <></>}
+      <select
+        className={"textfield" + (changed ? " changed" : "")}
+        value={props.value}
+        onChange={(event) => {
+          props.setValue(event.target.value as T);
+        }}
+        disabled={props.disabled}
+      >
+        {props.options.map((option) => (
+          <option key={option.value} value={option.value}>
+            {option.label}
+          </option>
+        ))}
+      </select>
+    </BaseSetting>
+  );
+}
