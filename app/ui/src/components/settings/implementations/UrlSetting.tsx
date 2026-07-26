b6ecc0e42024985c0c444c39106f5e67d05873cd
IJssel <ijssel@synapt.nl>
2026-07-26 23:07:25 +0200

    Added UrlSetting

---
+++ app/ui/src/components/settings/implementations/UrlSetting.tsx
@@ -0,0 +1,35 @@
+import { useRef } from "react";
+import { type JSX } from "react/jsx-runtime";
+
+import "../settings.less";
+import { BaseSetting, type BaseSettingProps } from "../BaseSetting";
+import { RestoreButton } from "../RestoreButton";
+
+type UrlProps = BaseSettingProps<string> & { placeholder?: string };
+
+export function UrlSetting(props: UrlProps): JSX.Element {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const changed = props.savedVal !== undefined && props.savedVal !== props.value;
+  const restore = (): void => {
+    if (props.savedVal === undefined) return;
+    props.setValue(props.savedVal);
+  };
+
+  return (
+    <BaseSetting {...props} changed={changed} inputRef={inputRef}>
+      {changed ? <RestoreButton onClick={restore} /> : <></>}
+      <input
+        className={"textfield" + (changed ? " changed" : "")}
+        type="url"
+        placeholder={props.placeholder}
+        ref={inputRef}
+        value={props.value}
+        onChange={(event) => {
+          props.setValue(event.target.value);
+        }}
+        disabled={props.disabled}
+      />
+    </BaseSetting>
+  );
+}
