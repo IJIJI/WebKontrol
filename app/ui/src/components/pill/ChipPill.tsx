c4b5986a36d100335751e4684302cedce1a5b0ce
IJssel <ijssel@synapt.nl>
2026-07-31 17:25:57 +0200

    Added chipPill

---
+++ app/ui/src/components/pill/ChipPill.tsx
@@ -0,0 +1,25 @@
+import { type ReactNode, type CSSProperties } from "react";
+import { type JSX } from "react/jsx-runtime";
+
+import "./chipPill.less";
+
+export function ChipPill({
+  children,
+  color,
+  size,
+  className,
+}: {
+  children: ReactNode;
+  color?: string;
+  size?: number;
+  className?: string;
+}): JSX.Element {
+  return (
+    <div
+      className={["chipPill", color && "colored", className].filter(Boolean).join(" ")}
+      style={{ fontSize: size, ...(color ? { "--chip-color": color } : {}) }}
+    >
+      {children}
+    </div>
+  );
+}
