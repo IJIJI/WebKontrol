06478b9226441309b828e9639d9fe09bb0daaba2
IJssel <ijssel@synapt.nl>
2026-07-31 17:42:41 +0200

    Updated chipPill

--- app/ui/src/components/views/ViewTypeChip.tsx
+++ app/ui/src/components/views/ViewTypeChip.tsx
@@ -1,17 +1,16 @@
 import { type JSX } from "react/jsx-runtime";
-import { InfoPill, PillType } from "../pill/InfoPill";
+import { ChipPill } from "../pill/ChipPill";
 import { type ViewType } from "../../../../src/views/types/schema";
 import { VIEW_TYPE_META } from "./viewMeta";
 
-// A grey chip showing a view's type (icon + label). Uses PillType.DEFAULT for now;
-// see the TODO in InfoPill about swapping to a base typeless chip once it exists.
+// A grey chip showing a view's type (icon + label), on the neutral ChipPill base.
 export function ViewTypeChip({ type }: { type: ViewType }): JSX.Element {
   const meta = VIEW_TYPE_META[type];
   const Icon = meta.icon;
   return (
-    <InfoPill type={PillType.DEFAULT}>
+    <ChipPill>
       <Icon size={13} />
       <span>{meta.label}</span>
-    </InfoPill>
+    </ChipPill>
   );
 }
