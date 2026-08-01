b5c9c747c14b22699e32bec793c434925241b0e2
IJssel <ijssel@synapt.nl>
2026-08-02 00:58:02 +0200

    Viewstatpill wording

--- app/ui/src/components/views/ViewStatusPill.tsx
+++ app/ui/src/components/views/ViewStatusPill.tsx
@@ -15,7 +15,7 @@ export function ViewStatusPill({ view, ...props }: { view: UiViewState; size?: n
 
   const activePuppets = view.activePuppets;
   const status = activePuppets.length <= 0 ? ConnectionState.DISABLED : ConnectionState.FAILED;
-  const label = activePuppets.length <= 0 ? "Not Displayed" : 
+  const label = activePuppets.length <= 0 ? "Inactive" : 
                   ( activePuppets.length == 1 ? state?.puppets.get(activePuppets[0])?.config.name.short ?? activePuppets[0] : `Active ${activePuppets.length}x` ); // TODO Simplify
   
   return (
