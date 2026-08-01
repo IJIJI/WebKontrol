04548134afa17800a26a06a130642a9767643585
IJssel <ijssel@synapt.nl>
2026-08-02 00:42:39 +0200

    Added more props

--- app/ui/src/components/pill/statusPill/ViewStatusPill.tsx
+++ app/ui/src/components/pill/statusPill/ViewStatusPill.tsx
@@ -2,11 +2,15 @@ import { JSX } from "react/jsx-runtime";
 import { UiViewState, useApi } from "../../../context/ApiStateContext";
 import { StatusPill } from "./StatusPill";
 import { ConnectionState } from "../../../../../src/types/CommonTypes";
+import { FillStyle } from "../../../helpers/variants";
 
 
+  
+    
 
 
-export function ViewStatusPill({ view }: { view: UiViewState }): JSX.Element {
+    
+export function ViewStatusPill({ view, ...props }: { view: UiViewState; size?: number; fillStyle?: FillStyle; collapsed?: boolean; }): JSX.Element {
   const { state } = useApi()
 
   const activePuppets = view.activePuppets;
@@ -15,6 +19,6 @@ export function ViewStatusPill({ view }: { view: UiViewState }): JSX.Element {
                   ( activePuppets.length == 1 ? state?.puppets.get(activePuppets[0])?.config.name.short ?? activePuppets[0] : `Active ${activePuppets.length}x` ); // TODO Simplify
   
   return (
-    <StatusPill status={status} label={label} />
+    <StatusPill {...props} status={status} label={label} />
   );
 }
