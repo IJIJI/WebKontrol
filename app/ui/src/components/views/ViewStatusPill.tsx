aa237e92fdc1edf4dfd0bacc86158dd42cea63cf
IJssel <ijssel@synapt.nl>
2026-08-03 00:29:18 +0200

    Made viewStatusPill a groupStatusPill

--- app/ui/src/components/views/ViewStatusPill.tsx
+++ app/ui/src/components/views/ViewStatusPill.tsx
@@ -1,6 +1,7 @@
 import { type JSX } from "react/jsx-runtime";
 import { type UiViewState, useApi } from "../../context/ApiStateContext";
 import { StatusPill } from "../pill/statusPill/StatusPill";
+import { GroupStatusPill, type StatusItem } from "../pill/statusPill/GroupStatusPill";
 import { ConnectionState } from "../../../../src/types/CommonTypes";
 import { type FillStyle } from "../../common/types/variants";
 
@@ -16,6 +17,7 @@ export function ViewStatusPill({
   const { state } = useApi();
 
   const assignedPuppets = view.assignedPuppets;
+
   const status = assignedPuppets.length <= 0 ? ConnectionState.DISABLED : ConnectionState.FAILED;
   const label =
     assignedPuppets.length <= 0
@@ -24,6 +26,19 @@ export function ViewStatusPill({
         ? state?.puppets.get(assignedPuppets[0])?.config.name.short ?? assignedPuppets[0]
         : `Active ${assignedPuppets.length}x`;
 
-  // TODO: Implement GroupStatusPill to show which puppets are active on hover / when collapsed.
-  return <StatusPill {...props} status={status} label={label} />;
+
+  const items: StatusItem[] = assignedPuppets.map((id) => {
+    const puppet = state?.puppets.get(id);
+    return {
+      id,
+      name: puppet?.config.name.long ?? id,
+      status: puppet?.info.state ?? ConnectionState.UNKNOWN,
+    };
+  });
+
+  return (
+    <GroupStatusPill items={items}>
+      <StatusPill {...props} status={status} label={label} />
+    </GroupStatusPill>
+  );
 }
