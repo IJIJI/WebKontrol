e5cb8d5622eff99f6b3d752c8606360f7d71ec23
IJssel <ijssel@synapt.nl>
2026-08-02 23:03:48 +0200

    VIEW activePuppets -> assignedPuppets

--- app/ui/src/components/views/ViewStatusPill.tsx
+++ app/ui/src/components/views/ViewStatusPill.tsx
@@ -1,19 +1,29 @@
-import { JSX } from "react/jsx-runtime";
-import { UiViewState, useApi } from "../../context/ApiStateContext";
+import { type JSX } from "react/jsx-runtime";
+import { type UiViewState, useApi } from "../../context/ApiStateContext";
 import { StatusPill } from "../pill/statusPill/StatusPill";
 import { ConnectionState } from "../../../../src/types/CommonTypes";
-import { FillStyle } from "../../common/types/variants";
+import { type FillStyle } from "../../common/types/variants";
 
-    
-export function ViewStatusPill({ view, ...props }: { view: UiViewState; size?: number; fillStyle?: FillStyle; collapsed?: boolean; }): JSX.Element {
-  const { state } = useApi()
+export function ViewStatusPill({
+  view,
+  ...props
+}: {
+  view: UiViewState;
+  size?: number;
+  fillStyle?: FillStyle;
+  collapsed?: boolean;
+}): JSX.Element {
+  const { state } = useApi();
 
-  const activePuppets = view.activePuppets;
-  const status = activePuppets.length <= 0 ? ConnectionState.DISABLED : ConnectionState.FAILED;
-  const label = activePuppets.length <= 0 ? "Inactive" : 
-                  ( activePuppets.length == 1 ? state?.puppets.get(activePuppets[0])?.config.name.short ?? activePuppets[0] : `Active ${activePuppets.length}x` ); // TODO Simplify
-  
-  return (
-    <StatusPill {...props} status={status} label={label} />
-  ); // TODO: Implement GroupStatusPill and use it to show what views are active on hover, with more then one active puppet or when collapsed.
+  const assignedPuppets = view.assignedPuppets;
+  const status = assignedPuppets.length <= 0 ? ConnectionState.DISABLED : ConnectionState.FAILED;
+  const label =
+    assignedPuppets.length <= 0
+      ? "Inactive"
+      : assignedPuppets.length === 1
+        ? state?.puppets.get(assignedPuppets[0])?.config.name.short ?? assignedPuppets[0]
+        : `Active ${assignedPuppets.length}x`;
+
+  // TODO: Implement GroupStatusPill to show which puppets are active on hover / when collapsed.
+  return <StatusPill {...props} status={status} label={label} />;
 }
--- app/ui/src/context/ApiStateContext.tsx
+++ app/ui/src/context/ApiStateContext.tsx
@@ -30,7 +30,7 @@ export interface UiPuppetState extends PuppetDataBundle {
 export interface UiViewState {
   key: ViewKey;
   config: AnyViewConfig;
-  activePuppets: PuppetKey[];
+  assignedPuppets: PuppetKey[];
   update: (config: AnyViewConfig) => Promise<void>;
   delete: () => Promise<void>;
   assign: (puppets: PuppetKey[]) => Promise<void>;
@@ -191,7 +191,7 @@ export function ApiStateProvider({
           const full: UiViewState = {
             key,
             config,
-            activePuppets: Object.entries(data.runtime.puppetOrchestrator.assignments).filter(([k, v]) => v == key).map(([k,v]) => k), // TODO: Simplify?
+            assignedPuppets: Object.entries(data.runtime.puppetOrchestrator.assignments).filter(([, v]) => v == key).map(([k]) => k), // TODO: Simplify?
             update: (next: AnyViewConfig): Promise<void> => viewUpdate(key, next),
             delete: (): Promise<void> => viewDelete(key),
             assign: (puppets: PuppetKey[]): Promise<void> =>
