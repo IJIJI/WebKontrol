5c773b066a9baa421c364d1ab625f46c23a2a303
IJssel <ijssel@synapt.nl>
2026-08-02 21:32:52 +0200

    Added common move todo

--- app/src/helpers/json.ts
+++ app/src/helpers/json.ts
@@ -16,6 +16,7 @@
  * // Manual usage
  * JSON.stringify(data, jsonReplacer);
  */
+// TODO: Move to common/helpers
 export function jsonReplacer(key: string, value: unknown): unknown {
   if (value instanceof Map) return Object.fromEntries(value);
   if (value instanceof Set) return [...value];
