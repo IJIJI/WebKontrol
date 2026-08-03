dc5c9acd0b9eef052d953f8bb835cf7d6faaabcf
IJssel <ijssel@synapt.nl>
2026-08-03 14:49:50 +0200

    Added new pickers to testpage

--- app/ui/src/pages/TestPage.tsx
+++ app/ui/src/pages/TestPage.tsx
@@ -1,8 +1,16 @@
 import { type JSX } from "react/jsx-runtime";
 import { Icons } from "../components/icons/Icons";
 import "./testPage.less";
+import { ColorSetting } from "../components/settings/implementations/ColorSetting";
+import { useState } from "react";
+import { IconSetting } from "../components/settings/implementations/IconSetting";
+import { SettingWidth } from "../components/settings/settingWidth";
 
 export default function TestPage(): JSX.Element {
+
+  const [color, setColor] = useState<string>();
+  const [icon, setIcon] = useState<string>();
+
   return (
     <div className="testPage">
       <h2>Icons</h2>
@@ -16,6 +24,10 @@ export default function TestPage(): JSX.Element {
           ),
         )}
       </div>
+      <ColorSetting title="Color" value={color} setValue={setColor} savedVal="#16b058"/>
+      <ColorSetting title="Color" value={color} setValue={setColor} savedVal="#16b058" width={SettingWidth.COMPACT}/>
+      <IconSetting title="Icon" subtitle="bladiebla" value={icon} setValue={setIcon} savedVal="cogs" />
+      <IconSetting title="Icon" value={icon} setValue={setIcon} savedVal="cogs" width={SettingWidth.COMPACT}/>
     </div>
   );
 }
