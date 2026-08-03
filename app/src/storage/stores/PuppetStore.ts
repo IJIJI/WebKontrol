8535a261381af03b786b4d728776b9b542e2965f
IJssel <ijssel@synapt.nl>
2026-08-04 01:24:53 +0200

    Fixed logging parsing and async

--- app/src/storage/stores/PuppetStore.ts
+++ app/src/storage/stores/PuppetStore.ts
@@ -49,19 +49,25 @@ export class PuppetStore {
   }
 
   public async clearRuntime(): Promise<void> {
-    this._db.deleteSetting("puppet", this._id, "runtime");
+    await this._db.deleteSetting("puppet", this._id, "runtime");
   }
 
   public async saveAppearance(appearance: EntityAppearance): Promise<void> {
+    this._logger.debug(`Saving appearance...`);
     await this._db.updateSetting("puppet", this._id, "appearance", JSON.stringify(appearance));
+    this._logger.debug(`Successfully saved appearance!`);
   }
 
   public async loadAppearance(): Promise<EntityAppearance | null> {
     try {
+      this._logger.debug(`Loading appearance...`);
       const raw = await this._db.getSetting("puppet", this._id, "appearance");
       if (raw === null) return null;
-      return EntityAppearanceSchema.parse(JSON.parse(raw));
-    } catch {
+      const object = EntityAppearanceSchema.parse(JSON.parse(raw));
+      this._logger.debug(`Successfully loaded appearance!`, object);
+      return object;
+    } catch (error) {
+      this._logger.info(`Failed loading appearance for puppet ${this._id} with error:`, error, `Returning null.`);
       return null;
     }
   }
